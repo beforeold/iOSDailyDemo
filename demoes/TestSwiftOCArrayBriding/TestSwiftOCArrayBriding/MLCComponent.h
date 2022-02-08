4ba82561d2798ee658cc3138d78120bff83bc972
BrookXy <brook.dinglan@lazada.com>
2022-02-08 19:45:31 +0800

    add TestOCMutableCopy and TestSwiftOCArrayBriding

---
+++ demoes/TestSwiftOCArrayBriding/TestSwiftOCArrayBriding/MLCComponent.h
@@ -0,0 +1,19 @@
+//
+//  MLCComponent.h
+//  TestSwiftOCArrayBridinging
+//
+//  Created by BrookXy on 2022/2/7.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MLCComponent : NSObject
+
+
+@property (nonatomic, copy) NSString *name;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ demoes/TestSwiftOCArrayBriding/TestSwiftOCArrayBriding/MLCParseResult.m
@@ -0,0 +1,12 @@
+//
+//  MLCParseResult.m
+//  TestSwiftOCArrayBridinging
+//
+//  Created by BrookXy on 2022/2/7.
+//
+
+#import "MLCParseResult.h"
+
+@implementation MLCParseResult
+
+@end
