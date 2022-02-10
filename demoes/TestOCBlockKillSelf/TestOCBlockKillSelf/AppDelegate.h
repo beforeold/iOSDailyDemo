46588944bc96ce8ad2efa5605e111894feb7b81f
BrookXy <brook.dinglan@lazada.com>
2022-02-10 16:36:36 +0800

    TestOCBlockKillSelf

---
+++ demoes/TestOCBlockKillSelf/TestOCBlockKillSelf/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TestOCBlockKillSelf
+//
+//  Created by BrookXy on 2022/2/10.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ demoes/TestOCBlockKillSelf/TestOCBlockKillSelf/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TestOCBlockKillSelf
+//
+//  Created by BrookXy on 2022/2/10.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
