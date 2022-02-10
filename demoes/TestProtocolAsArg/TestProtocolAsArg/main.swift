d44752d7c5e560b240e38539abbb601ac706441a
BrookXy <brook.dinglan@lazada.com>
2022-02-10 15:50:27 +0800

    add TestProtocolAsArg

--- demoes/TestProtocolAsArg/TestProtocolAsArg/main.swift
+++ demoes/TestProtocolAsArg/TestProtocolAsArg/main.swift
@@ -11,24 +11,59 @@ protocol Talkable {
     func talk()
 }
 
-struct Brook: Talkable {
+extension Talkable {
+    func talkTwice() {
+        talk()
+        talk()
+    }
+}
+
+struct Brook1: Talkable {
     func talk() {
-        print("brook talk")
+        print("brook1 talk")
+    }
+}
+
+struct Brook2: Talkable {
+    func talk() {
+        print("brook2 talk")
+    }
+    
+    func talkTwice() {
+        print("brook talk twice")
     }
 }
 
 func unkownTypeTalk(by talker: Talkable) {
-    print("talker", talker)
+    print("talker unkown", talker)
     talker.talk()
+    talker.talkTwice()
 }
 
 func genericTalk<T>(by talker: T) where T: Talkable {
-    print("talker", talker)
+    print("talker generic", talker)
     talker.talk()
+    talker.talkTwice()
 }
 
-unkownTypeTalk(by: Brook())
+
+// just the same
+
+unkownTypeTalk(by: Brook1())
 print()
-genericTalk(by: Brook())
+genericTalk(by: Brook2())
 
 
+
+protocol Parentable {
+    associatedtype Child
+    func whichChild() -> Child.Type
+}
+
+// fail to build
+// func testParent1(parent: Parentable)
+
+// ok to build
+func testParent2<T>(parent: T) where T: Parentable {
+    
+}
