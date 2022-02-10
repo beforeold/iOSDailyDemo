f1345a94d3d5f8f84df1fcb698eeefb58c29496c
BrookXy <brook.dinglan@lazada.com>
2022-02-10 16:36:36 +0800

    TestOCBlockKillSelf

---
+++ demoes/TestOCBlockKillSelf/TestOCBlockKillSelf/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  TestOCBlockKillSelf
+//
+//  Created by BrookXy on 2022/2/10.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ demoes/TestOCBlockKillSelf/TestOCBlockKillSelf/ViewController.m
@@ -0,0 +1,68 @@
+//
+//  ViewController.m
+//  TestOCBlockKillSelf
+//
+//  Created by BrookXy on 2022/2/10.
+//
+
+#import "ViewController.h"
+
+@interface BaseMananger : NSObject
+
+@property (nullable, nonatomic, copy) dispatch_block_t handler;
+
+- (void)actionWithCompletionHandler:(dispatch_block_t)handler;
+
+@end
+
+@implementation BaseMananger
+
+- (void)actionWithCompletionHandler:(dispatch_block_t)handler {
+    self.handler = handler;
+    [self callback];
+}
+
+- (void)callback {
+    self.handler();
+    self.handler = nil;
+}
+
+- (void)dealloc {
+    NSLog(@"BaseMananger dealloc");
+}
+
+@end
+
+@interface MyMananger : BaseMananger
+
+@end
+
+@implementation MyMananger
+
+- (void)actionWithCompletionHandler:(dispatch_block_t)handler {
+    [super actionWithCompletionHandler:handler];
+    [self description];
+}
+
+@end
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+     __block __auto_type ins = [[MyMananger alloc] init];
+    [ins actionWithCompletionHandler:^{
+        NSLog(@"completion");
+        
+        // unsafe to nil-out self synchrously
+        ins = nil;
+    }];
+}
+
+
+@end
