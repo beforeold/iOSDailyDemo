8417f17880807ddb3880e7f69aee4d49f5d5a887
BrookXy <brook.dinglan@lazada.com>
2022-02-10 15:06:21 +0800

    TestKaleidoscopeWrap2 added

---
+++ demoes/TestKaleidoscopeWrap2/TestKaleidoscopeWrap2/KSTextField.h
@@ -0,0 +1,20 @@
+//
+//  KSTextField.h
+//  TestKaleidoscopeWrap2
+//
+//  Created by BrookXy on 2022/2/10.
+//
+
+@import UIKit;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KSTextField : NSObject
+
+- (CGRect)borderRectForBounds:(CGRect)bounds;
+
+@property (class, nonatomic) BOOL enableUIKIt;
+
+@end
+
+NS_ASSUME_NONNULL_END
