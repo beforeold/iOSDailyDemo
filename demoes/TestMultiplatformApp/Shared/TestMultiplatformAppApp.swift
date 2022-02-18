b46a12906166ea6f900aaad19e32d392a8457374
BrookXy <brook.dinglan@lazada.com>
2022-02-18 13:48:05 +0800

    add TestSwiftObjcDynamic and TestMultiplatformApp

---
+++ demoes/TestMultiplatformApp/Shared/TestMultiplatformAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMultiplatformAppApp.swift
+//  Shared
+//
+//  Created by BrookXy on 2022/2/18.
+//
+
+import SwiftUI
+
+@main
+struct TestMultiplatformAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
