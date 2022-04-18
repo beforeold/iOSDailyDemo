eb2a646107f6bf5557c5a2dc603036d54e9a4275
BrookXy <brook.dinglan@lazada.com>
2022-04-18 16:15:39 +0800

    update TestNSNullBridging; add TestNewProjectDefaultTargetOSVersion

---
+++ demoes/TestNewProjectDefaultTargetOSVersion/TestNewProjectDefaultTargetOSVersion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestNewProjectDefaultTargetOSVersion
+//
+//  Created by BrookXy on 2022/4/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ demoes/TestNewProjectDefaultTargetOSVersion/TestNewProjectDefaultTargetOSVersion/TestNewProjectDefaultTargetOSVersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNewProjectDefaultTargetOSVersionApp.swift
+//  TestNewProjectDefaultTargetOSVersion
+//
+//  Created by BrookXy on 2022/4/18.
+//
+
+import SwiftUI
+
+@main
+struct TestNewProjectDefaultTargetOSVersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
