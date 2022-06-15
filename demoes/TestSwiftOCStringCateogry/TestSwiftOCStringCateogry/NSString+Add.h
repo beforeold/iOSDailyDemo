fb2258da19dc0130009de3b91db5a7824b8edf42
BrookXy <cirpxpp@163.com>
2022-06-15 16:31:08 +0800

    add TestSwiftOCStringCateogry

---
+++ demoes/TestSwiftOCStringCateogry/TestSwiftOCStringCateogry/NSString+Add.h
@@ -0,0 +1,20 @@
+//
+//  NSString+Add.h
+//  TestSwiftOCStringCateogry
+//
+//  Created by BrookXy on 2022/6/15.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (Add)
+
+- (NSString *)add_foo;
+
+- (void)barBarBar;
+
+@end
+
+NS_ASSUME_NONNULL_END
