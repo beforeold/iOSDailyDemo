7bb49ebc22c008d4adca276b259e1f6ba3f5aa8f
beforeold <ibeforeold@gmail.com>
2022-09-28 21:21:31 +0800

    add TestOCInstanceMethodClassMethod

---
+++ sources/TestOCInstanceMethodClassMethod/TestOCInstanceMethodClassMethod/Person.h
@@ -0,0 +1,20 @@
+//
+//  Person.h
+//  TestOCInstanceMethodClassMethod
+//
+//  Created by beforeold on 2022/9/28.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
++ (void)showPointer;
+
+- (void)speek;
+
+@end
+
+NS_ASSUME_NONNULL_END
