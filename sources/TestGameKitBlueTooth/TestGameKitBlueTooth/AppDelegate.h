2c22c6b290755a1d97de0a3ea286331f38c85e35
beforeold <ibeforeold@gmail.com>
2022-10-09 16:02:27 +0800

    add TestGameKitBlueTooth

---
+++ sources/TestGameKitBlueTooth/TestGameKitBlueTooth/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TestGameKitBlueTooth
+//
+//  Created by beforeold on 2022/10/9.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ sources/TestGameKitBlueTooth/TestGameKitBlueTooth/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  TestGameKitBlueTooth
+//
+//  Created by beforeold on 2022/10/9.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
