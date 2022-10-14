9568fd257095c0b4352e66dfa73e5ac8fa68653e
beforeold <ibeforeold@gmail.com>
2022-10-14 18:07:04 +0800

    add ProfileViewController

---
+++ sources/TestRxSwift/TestRxSwift/ProfileViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ProfileViewController.swift
+//  TestRxSwift
+//
+//  Created by beforeold on 2022/10/14.
+//
+
+import UIKit
+import RxSwift
+import RxRelay
+import RxCocoa
+
+class ProfileViewController: UIViewController {
+  
+  enum Gender {
+    case unknown
+    case male
+    case female
+  }
+  
+  @IBOutlet weak var female: UIButton!
+  @IBOutlet weak var swiftLevel: UISlider!
+  @IBOutlet weak var knowSwift: UISwitch!
+  @IBOutlet weak var male: UIButton!
+  @IBOutlet weak var datePicker: UIDatePicker!
+  @IBOutlet weak var passionToLearn: UIStepper!
+  
+  @IBOutlet weak var update: UIButton!
+  
+  var bag = DisposeBag()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    datePicker.layer.borderWidth = 1
+    
+    let datePub = datePicker.rx.date.map(Validator.isValidDate(date:))
+    
+    datePub.map { $0 ? UIColor.green : .clear }
+      .observe { value in
+        self.datePicker.layer.borderColor = value.cgColor
+      }
+      .disposed(by: bag)
+    
+    let genderSelection = BehaviorSubject<Gender>(value: .unknown)
+    
+    self.male.rx.tap
+      .map { return Gender.male }
+      .bind(to: genderSelection).disposed(by: bag)
+    
+    self.female.rx.tap
+      .map { return Gender.female }
+      .bind(to: genderSelection)
+      .disposed(by: bag)
+    
+    genderSelection.observe { gender in
+      switch gender {
+      case .male:
+        self.male.setTitleColor(.green, for: .normal)
+        self.female.setTitleColor(.blue, for: .normal)
+        
+      case .female:
+        self.male.setTitleColor(.blue, for: .normal)
+        self.female.setTitleColor(.green, for: .normal)
+        
+      case .unknown:
+        break
+      }
+    }.disposed(by: bag)
+    
+    let genderSelected = genderSelection.map { $0 != .unknown }
+    
+    Observable.combineLatest(genderSelected, datePub)
+      .map { $0.0 && $0.1 }
+      .bind(to: self.update.rx.isEnabled)
+      .disposed(by: bag)
+  }
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destination.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+}
