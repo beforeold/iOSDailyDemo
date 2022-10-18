4e21cc24fa3c037c4bcaf081641fa517b95213f3
beforeold <ibeforeold@gmail.com>
2022-10-18 18:23:51 +0800

    add TestHelloWorldC

---
+++ sources/TestHelloWorldC/TestHelloWorldC/MainFile.c
@@ -0,0 +1,13 @@
+//
+//  MainFile.c
+//  TestHelloWorldC
+//
+//  Created by beforeold on 2022/10/18.
+//
+
+#include <stdio.h>
+
+    int main(int argc, const char * argv[]) {
+        printf("Hello, World!\n");
+        return 0;
+    }
