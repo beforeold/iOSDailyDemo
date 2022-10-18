8a295e047e8e403cc09880e14a05e36e3f459b58
beforeold <ibeforeold@gmail.com>
2022-10-18 18:23:51 +0800

    add TestHelloWorldC

---
+++ sources/TestHelloWorldC/TestHelloWorldC/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  TestHelloWorldC
+//
+//  Created by beforeold on 2022/10/18.
+//
+
+#include <stdio.h>
+
+    main( ) {
+        extern a, b, c;
+        putchar(a); putchar(b); putchar(c); putchar('!*n');
+    }
+     
+    a 'hell';
+    b 'o, w';
+    c 'orld';
+
+
