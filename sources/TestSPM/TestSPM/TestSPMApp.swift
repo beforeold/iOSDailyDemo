4023582719c362711ba222d4e0258bc5e5d9d601
Brook <beforeold@163.com>
2022-11-01 17:01:43 +0800

    add TestSPM

---
+++ sources/TestSPM/TestSPM/TestSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSPMApp.swift
+//  TestSPM
+//
+//  Created by Brook_Mobius on 2022/11/1.
+//
+
+import SwiftUI
+
+@main
+struct TestSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
