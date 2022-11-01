0324ecbe970a626e460ca9175381b3cbf551fd60
Brook <beforeold@163.com>
2022-11-01 17:01:43 +0800

    add TestSPM

---
+++ sources/TestSPM/TestSPM/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  TestSPM
+//
+//  Created by Brook_Mobius on 2022/11/1.
+//
+
+import SwiftUI
+import RxCocoa
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
