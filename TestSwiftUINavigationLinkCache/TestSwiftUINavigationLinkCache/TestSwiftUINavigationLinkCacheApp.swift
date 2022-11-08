d9177212ff787c932e0a0ba0b53fc0ba0e5c0507
brook <brook@dayuwuxian.com>
2022-11-08 19:23:26 +0800

    add TestSwiftUINavigationLinkCache

---
+++ TestSwiftUINavigationLinkCache/TestSwiftUINavigationLinkCache/TestSwiftUINavigationLinkCacheApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUINavigationLinkCacheApp.swift
+//  TestSwiftUINavigationLinkCache
+//
+//  Created by Brook_Mobius on 2022/11/8.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUINavigationLinkCacheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
