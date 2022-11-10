e36ad0ac77d1e60ab1893895f46f9bef335b884a
beforeold <ibeforeold@gmail.com>
2022-11-10 22:32:18 +0800

    demo Effect of Redux

--- sources/TestRedux/TestRedux/ContentView.swift
+++ sources/TestRedux/TestRedux/ContentView.swift
@@ -9,14 +9,35 @@ import SwiftUI
 
 fileprivate enum Action {
   case tap
+  case later
+  case laterCallback(Int)
 }
 
 fileprivate func reducer(
   _ state: inout Int,
   _ action: Action
 ) -> Effect<Action>? {
-  state += 10
-  return nil
+  switch action {
+  case .tap:
+    state += 10
+    return .none
+    
+  case .later:
+    return makeLaterPromise(state: state)
+    
+  case .laterCallback(let ret):
+    state = ret
+    return .none
+  }
+}
+
+fileprivate func makeLaterPromise(state: Int) -> Promise<Action> {
+  let promise = Promise<Action>()
+  DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+    promise.resolve(with: .laterCallback(1000 + state))
+  }
+  
+  return promise
 }
 
 fileprivate let store = Store<Int, Action>(
@@ -33,6 +54,10 @@ struct ContentView: View {
         Text("value: \(viewStore.state)")
         Button("Tap") {
           viewStore.send(.tap)
+        }
+        
+        Button("Later Update") {
+          viewStore.send(.later)
         }
       }
     }
