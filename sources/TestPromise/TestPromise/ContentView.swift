4e8004ebc40dc6d27c3f5690ab9d488700ba5928
brook <brook@dayuwuxian.com>
2022-11-11 11:53:59 +0800

    commit testPromise

--- sources/TestPromise/TestPromise/ContentView.swift
+++ sources/TestPromise/TestPromise/ContentView.swift
@@ -7,20 +7,47 @@
 
 import SwiftUI
 
+class Promise<T> {
+  typealias promise = (T) -> Void
+  let builder: (@escaping promise) -> Void
+  init(builder: @escaping (@escaping promise) -> Void) {
+    self.builder = builder
+  }
+  
+  func run(callback: @escaping (T) -> Void) {
+    builder(callback)
+  }
+}
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+  @State var count = 0
+  var body: some View {
+    VStack {
+      Text("count: \(count)")
+      Button("Tap") {
+        let count = count
+        Promise { promise in
+          DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            promise(count + 666)
+          }
         }
-        .padding()
+        .run { int in
+          self.count = int
+        }
+      }
     }
+    .padding()
+  }
+}
+
+func foo() {
+  _ = Promise { callback in
+    callback(5)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
