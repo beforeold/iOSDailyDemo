35407862e071c649bdb6737473c5a81efe695449
brook <brook@dayuwuxian.com>
2022-11-11 11:07:57 +0800

    add TestPromise

---
+++ sources/TestPromise/TestPromise/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TestPromise
+//
+//  Created by Brook_Mobius on 2022/11/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
