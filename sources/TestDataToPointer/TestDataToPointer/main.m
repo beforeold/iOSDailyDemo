0fe3b3a3f49676fa6ca1aaa63e622d73323feb70
brook <brook@dayuwuxian.com>
2022-12-01 09:25:34 +0800

    fix data to pointer issue

--- sources/TestDataToPointer/TestDataToPointer/main.m
+++ sources/TestDataToPointer/TestDataToPointer/main.m
@@ -7,6 +7,8 @@
 
 #import <Foundation/Foundation.h>
 
+#define let __auto_type
+
 static const NSInteger kBytesLength = 8;
 
 @interface PhashValue : NSObject
@@ -64,7 +66,6 @@ static const NSInteger kBytesLength = 8;
 
 
 void testList(void) {
-  int list[8] = {1, 2, 3, 4, 5, 6, 7, 8};
   char *str = "12345678";
   
   __auto_type data = [NSData dataWithBytes:str length:8];
@@ -121,12 +122,81 @@ void testList3(void) {
   }
 }
 
+void testList4(void) {
+  let intCount = 8;
+  let byteLength = intCount * sizeof(int);
+  int *list = (int *)malloc(byteLength);
+  
+  for (int i = 0; i < 8; i++) {
+    list[i] = i + 1;
+  }
+
+  let data = [NSData dataWithBytes:list length:byteLength];
+  
+  NSLog(@"sizeof int: %lu", sizeof(int));
+  NSLog(@"sizeof uint8_t: %lu", sizeof(uint8_t));
+  NSLog(@"sizeof Byte: %lu", sizeof(Byte));
+  
+  NSUInteger len = [data length];
+  Byte *byteData = (Byte *)malloc(len);
+  memcpy(byteData, [data bytes], len);
+  
+  for (int i = 0; i < data.length; i++) {
+    printf("value: %d\n", byteData[i]);
+  }
+}
+
+
+void testList5_ok(void) {
+  let intCount = 8;
+  let byteLength = intCount * sizeof(int);
+  int *list = (int *)malloc(byteLength);
+  
+  for (int i = 0; i < 8; i++) {
+    list[i] = i + 1;
+  }
+
+  let data = [NSData dataWithBytes:list length:byteLength];
+  
+  int *byteData = (int *)data.bytes;
+  
+  for (int i = 0; i < intCount; i++) {
+    printf("value: %d\n", byteData[i]);
+  }
+}
+
+
+void testPhash(void) {
+  int *list = (int *)malloc(8 * sizeof(int));
+  
+  for (int i = 0; i < 8; i++) {
+    list[i] = i + 1;
+  }
+  
+  let data1 = [NSData dataWithBytes:list length:8];
+  let phash1 = [[PhashValue alloc] initWithData:data1];
+  let condition = [data1 isEqual:phash1.data];
+  NSLog(@"ret %d", condition);
+  
+  let pointer = phash1.pointer;
+  for (int i = 0; i < 8; i++) {
+    NSLog(@"value: %d", pointer[i]);
+  }
+  
+  {
+    let phash2 = [[PhashValue alloc] initWithPointer:list];
+    let pointer = phash2.pointer;
+    for (int i = 0; i < 8; i++) {
+      NSLog(@"value: %d", pointer[i]);
+    }
+  }
+}
 
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
     // insert code here...
     
-    testList3();
+    testList5_ok();
     
   }
   return 0;
