0de873e5f18b6b6fed919da0134fd35b6113f913
beforeold <ibeforeold@gmail.com>
2022-12-12 21:43:33 +0800

    add TestPublisherAssignTo

---
+++ sources/TestPublisherAssignTo/TestPublisherAssignTo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestPublisherAssignTo
+//
+//  Created by beforeold on 2022/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
