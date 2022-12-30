e46c4fe3746226741e39aecaaeb8601179cbbfcd
brook <brook@dayuwuxian.com>
2022-12-30 09:35:51 +0800

    add TestSwiftUIGradient

---
+++ sources/TestSwiftUIGradient/TestSwiftUIGradient/TestSwiftUIGradientApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUIGradientApp.swift
+//  TestSwiftUIGradient
+//
+//  Created by Brook_Mobius on 2022/12/30.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUIGradientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
