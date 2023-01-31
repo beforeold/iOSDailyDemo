f1b036770ceb9164a58005d046adb0471bc77392
beforeold <ibeforeold@gmail.com>
2023-01-31 16:08:56 +0800

    fix TestAutoLayoutWithAnimation with self.view.layoutIfNeeded instead of self.grayView.layoutIfNeeded

--- sources/TestAutoLayoutWithAnimation/TestAutoLayoutWithAnimation/SomeView.swift
+++ sources/TestAutoLayoutWithAnimation/TestAutoLayoutWithAnimation/SomeView.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class SomeView: UIView {
 
+  var button: UIView!
+  
+  var usesFrameLayout = false
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
     
@@ -22,35 +26,49 @@ class SomeView: UIView {
   override func layoutSubviews() {
     super.layoutSubviews()
     
+    if usesFrameLayout {
+      applyFrameLayout()
+    }
+    
     print(subviews[0].frame)
   }
   
   func setupUI() {
-    let button = UIButton(type: .custom)
-    button.setTitle("Click", for: .normal)
+    button = UIView()
+    button.backgroundColor = .yellow
     addSubview(button)
     
+    if !usesFrameLayout {
+      applyAutoLayout()
+    }
+  }
+  
+  func applyFrameLayout() {
+    button.frame = CGRect(x: 10, y: 10, width: bounds.width - 10 * 2, height: bounds.height - 10 * 2)
+  }
+  
+  func applyAutoLayout() {
     button.translatesAutoresizingMaskIntoConstraints = false
-    button.backgroundColor = .yellow
     
-//    button.frame = CGRect(x: 10, y: 10, width: 80, height: 80)
+    let trailing = {
+      let ret = button.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10)
+      ret.priority = .defaultHigh
+      return ret
+    }()
+    
+    let bottom = {
+      let ret = button.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10)
+      ret.priority = .defaultHigh
+      return ret
+    }()
     
     NSLayoutConstraint.activate([
       button.topAnchor.constraint(equalTo: topAnchor, constant: 10),
       button.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
-      {
-        let ret = button.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10)
-        ret.priority = .defaultHigh
-        return ret
-      }(),
-      // button.heightAnchor.constraint(equalToConstant: 80),
-      {
-        let ret = button.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10)
-        ret.priority = .defaultHigh
-        return ret
-      }(),
+      trailing,
+      bottom,
     ])
   }
   
-  static var viewHeght: CGFloat = 100
+  static var viewHeght: CGFloat = 200
 }
