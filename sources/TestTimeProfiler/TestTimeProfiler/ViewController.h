e633ec0c5ea2686a2b81eb0829c003e979618ca5
beforeold <ibeforeold@gmail.com>
2023-02-02 11:32:28 +0800

    add TestTimeProfiler

---
+++ sources/TestTimeProfiler/TestTimeProfiler/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TestTimeProfiler
+//
+//  Created by Brook_Mobius on 2023/2/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sources/TestTimeProfiler/TestTimeProfiler/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  TestTimeProfiler
+//
+//  Created by Brook_Mobius on 2023/2/2.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view.
+}
+
+
+@end
