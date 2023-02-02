d8e4df9ba475227e46e0d0c55cb25b3cb5275bfa
beforeold <ibeforeold@gmail.com>
2023-02-02 11:32:28 +0800

    add TestTimeProfiler

---
+++ sources/TestTimeProfiler/TestTimeProfiler/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TestTimeProfiler
+//
+//  Created by Brook_Mobius on 2023/2/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ sources/TestTimeProfiler/TestTimeProfiler/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  TestTimeProfiler
+//
+//  Created by Brook_Mobius on 2023/2/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
