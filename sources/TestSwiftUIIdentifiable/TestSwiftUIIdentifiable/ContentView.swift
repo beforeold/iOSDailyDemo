b8ffbbc6bcf7c6773b49376d71cae39c36931e31
beforeold <ibeforeold@gmail.com>
2023-04-02 23:28:33 +0800

    add TestSwiftLaz and TestSwiftUIIdentifiabl

---
+++ sources/TestSwiftUIIdentifiable/TestSwiftUIIdentifiable/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  TestSwiftUIIdentifiable
+//
+//  Created by Brook_Mobius on 2023/4/2.
+//
+
+import SwiftUI
+
+class Person: Identifiable {
+  var id: UUID = .init()
+  
+  var age: Int = 0
+}
+
+struct ContentView: View {
+  
+  @State var persons: [Person] = [
+    .init(),
+    .init(),
+    .init(),
+    .init(),
+  ]
+  
+  private func update(person: Person) {
+//     self.persons = persons
+//     self.persons[0].age += 10
+//    return
+    
+    guard let index = persons.firstIndex(where: { p in
+      p.id == person.id
+    }) else {
+      return
+    }
+    
+    var person = self.persons[index]
+    person.age += 10
+//    self.persons[index] = person
+//    self.persons[index] = person
+    
+//    withAnimation {
+//      self.persons[index] = person
+//    }
+  }
+  
+  var body: some View {
+    return content
+  }
+  
+  var content: some View {
+    List(persons) { person in
+      Text("age: \(person.age)")
+        .onTapGesture {
+          self.update(person: person)
+        }
+    }
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
