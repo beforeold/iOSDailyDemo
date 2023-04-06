69560478f9e1ca1b9cd6f61330b87564e5a8a843
beforeold <ibeforeold@gmail.com>
2023-04-06 19:32:23 +0800

    add TestAppStorage

---
+++ sources/TestAppStorage/TestAppStorage/HomeView.swift
@@ -0,0 +1,60 @@
+//
+//  HomeView.swift
+//  TestAppStorage
+//
+//  Created by Brook_Mobius on 2023/4/6.
+//
+
+import SwiftUI
+
+class ViewModel: ObservableObject {
+  @AppStorage("settings.count")
+  var count: Int = 0 {
+    didSet {
+      print("")
+    }
+  }
+  
+  static let shared: ViewModel = .init()
+}
+
+struct HomeView: View {
+
+  var body: some View {
+    Text("count: \(countValue)")
+      .onTapGesture {
+        self.countValue += 10
+      }
+  }
+  
+  // MARK: - using property
+  @AppStorage("settings.count") var count: Int = 0
+  var countValue: Int {
+    get {
+      count
+    }
+
+    nonmutating set {
+      count = newValue
+    }
+  }
+  
+  // MARK: - using ViewModel
+//  @StateObject var viewModel = ViewModel.shared
+//
+//  var countValue: Int {
+//    get {
+//      viewModel.count
+//    }
+//
+//    nonmutating set {
+//      viewModel.count = newValue
+//    }
+//  }
+}
+
+struct HomeView_Previews: PreviewProvider {
+  static var previews: some View {
+    HomeView()
+  }
+}
---
+++ sources/TestAppStorage/TestAppStorage/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  TestAppStorage
+//
+//  Created by Brook_Mobius on 2023/4/6.
+//
+
+import SwiftUI
+
+class ViewController: UIHostingController<HomeView> {
+
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder, rootView: HomeView())
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(onTick), userInfo: nil, repeats: true)
+    
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(onUserDefaultsChange(_:)),
+      name: UserDefaults.didChangeNotification,
+      object: nil
+    )
+  }
+  
+  @objc func onUserDefaultsChange(_ note: Notification) {
+    print(#function, Thread.current, note)
+  }
+  
+  @objc func onTick() {
+    DispatchQueue.global().async {
+      let key = "settings.count"
+      var value = UserDefaults.standard.integer(forKey: key)
+      value += 100
+      UserDefaults.standard.set(value, forKey: key)
+    }
+  }
+
+
+}
