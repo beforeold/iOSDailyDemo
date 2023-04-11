38357181d72ae0875fc1739b73c4d804865b9b81
beforeold <ibeforeold@gmail.com>
2023-04-11 12:22:31 +0800

    add TestATT

---
+++ sources/TestATTWithSceneDelegate/TestATT/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  TestATT
+//
+//  Created by Brook_Mobius on 2023/4/11.
+//
+
+import UIKit
+import AppTrackingTransparency
+
+class ViewController: UIViewController {
+  
+  var button: UIButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    button = UIButton(type: .custom)
+    button.frame = .init(x: 100, y: 100, width: 200, height: 100)
+    button.setTitleColor(.blue, for: .normal)
+    button.setTitle("Click to request", for: .normal)
+    button.titleLabel?.numberOfLines = 0
+    button.addTarget(self, action: #selector(onClick), for: .touchUpInside)
+    view.addSubview(button)
+  }
+  
+  @objc func onClick() {
+    ATTrackingManager.requestTrackingAuthorization { status in
+      print("staus", status.rawValue)
+      DispatchQueue.main.async {
+        self.button.setTitle("Click to request, ret \(status.rawValue)", for: .normal)
+      }
+    }
+  }
+  
+}
+
