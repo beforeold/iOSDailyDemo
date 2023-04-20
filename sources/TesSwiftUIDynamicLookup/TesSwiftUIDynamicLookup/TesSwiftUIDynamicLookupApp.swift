f5f17046108f087651c2c38d079d94466fb5d43e
beforeold <ibeforeold@gmail.com>
2023-04-21 07:07:25 +0800

    add TesSwiftUIDynamicLookup

---
+++ sources/TesSwiftUIDynamicLookup/TesSwiftUIDynamicLookup/TesSwiftUIDynamicLookupApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesSwiftUIDynamicLookupApp.swift
+//  TesSwiftUIDynamicLookup
+//
+//  Created by beforeold on 21/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TesSwiftUIDynamicLookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+          ContentView(viewModel: .init())
+        }
+    }
+}
