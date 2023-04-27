6c891a1719a15ef24244d7ef5447da4475699944
beforeold <ibeforeold@gmail.com>
2023-04-27 14:17:42 +0800

    add TestNewInfoPlist

---
+++ sources/TestNewInfoPlist/TestNewInfoPlist/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TestNewInfoPlist
+//
+//  Created by Brook_Mobius on 4/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sources/TestNewInfoPlist/TestNewInfoPlist/TestNewInfoPlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNewInfoPlistApp.swift
+//  TestNewInfoPlist
+//
+//  Created by Brook_Mobius on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct TestNewInfoPlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
