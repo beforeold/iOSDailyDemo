589da5e079c224f50fd53c9485a6f827f12c75a4
beforeold <ibeforeold@gmail.com>
2023-04-28 10:04:36 +0800

    add TestLibSwiftConcurrency

---
+++ sources/TestLibSwiftConcurrency/TestLibSwiftConcurrency/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  TestLibSwiftConcurrency
+//
+//  Created by Brook_Mobius on 4/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundColor(.accentColor)
+      Text("Hello, world!")
+    }
+    .padding()
+    .onAppear {
+      self.foo()
+    }
+  }
+  
+  @MainActor
+  func foo() {
+    Task {
+      await bar()
+    }
+  }
+  
+  func bar() async {
+    print("bar")
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ sources/TestLibSwiftConcurrency/TestLibSwiftConcurrency/TestLibSwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestLibSwiftConcurrencyApp.swift
+//  TestLibSwiftConcurrency
+//
+//  Created by Brook_Mobius on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct TestLibSwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
