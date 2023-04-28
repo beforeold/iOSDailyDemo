18ffd9dc04b984945c7cc1bc9893c153f93786fb
beforeold <ibeforeold@gmail.com>
2023-04-28 12:22:17 +0800

    add more tests for AppStorage

--- sources/TestAppStorage/TestAppStorage/HomeView.swift
+++ sources/TestAppStorage/TestAppStorage/HomeView.swift
@@ -40,9 +40,23 @@ let sharedBinding: Binding<Bool> = .init {
 }
 
 
+struct CustomView: View {
+  @Binding var flag: Bool
+  
+  var body: some View {
+    Text(flag ? "TTTTT" : "FFFF")
+  }
+}
+
 struct HomeView: View {
 
   @Binding var flag: Bool
+  
+  @State var flag2: Bool = false {
+    didSet {
+      print("did set", Thread.current)
+    }
+  }
   
   var body: some View {
     VStack(spacing: 20) {
@@ -59,8 +73,30 @@ struct HomeView: View {
         .onTapGesture {
           self.countValue += 10
         }
+      
+      
+      // 在 view 上直接使用 @State 或者 @AppStorage 的处理，系统会处理线程问题
+      CustomView(flag: $flag2)
+        .onTapGesture {
+//          self.flag2 = true
+//          return
+          DispatchQueue.global().async {
+            self.flag2 = true
+          }
+        }
+      
+      // 通过 viewModel 进行 published 处理，刷新 UI 会有警告
+      // Publishing changes from background threads is not allowed; make sure to publish values from the main thread (via operators like receive(on:)) on model updates.
+      CustomView(flag: $viewModel.flag)
+        .onTapGesture {
+//          self.flag2 = true
+//          return
+          DispatchQueue.global().async {
+            viewModel.flag.toggle()
+          }
+        }
     }
-    .background(Color.gray)
+    .background(Color(UIColor.lightGray.withAlphaComponent(0.2)))
   }
   
   // MARK: - using property
@@ -77,7 +113,7 @@ struct HomeView: View {
   }
   
   // MARK: - using ViewModel
-//  @StateObject var viewModel = ViewModel.shared
+  @StateObject var viewModel = ViewModel.shared
 //
 //  var countValue: Int {
 //    get {
