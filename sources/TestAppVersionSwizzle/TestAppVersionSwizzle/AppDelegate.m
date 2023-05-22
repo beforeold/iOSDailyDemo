9866ad3f2ee9f83b02c43745b06cad8293f3a278
beforeold <ibeforeold@gmail.com>
2023-05-22 15:58:19 +0800

    add TestAppVersionSwizzle

---
+++ sources/TestAppVersionSwizzle/TestAppVersionSwizzle/AppDelegate.m
@@ -0,0 +1,47 @@
+//
+//  AppDelegate.m
+//  TestAppVersionSwizzle
+//
+//  Created by Brook_Mobius on 5/22/23.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  
+  
+  NSLog(@"%@", NSBundle.mainBundle.infoDictionary);
+  
+  NSLog(@"");
+  
+  NSLog(@"%@", NSBundle.mainBundle.localizedInfoDictionary);
+  
+  return YES;
+}
+
+
+#pragma mark - UISceneSession lifecycle
+
+
+- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
+  // Called when a new scene session is being created.
+  // Use this method to select a configuration to create the new scene with.
+  return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+}
+
+
+- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
+  // Called when the user discards a scene session.
+  // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+  // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+}
+
+
+@end
---
+++ sources/TestAppVersionSwizzle/TestAppVersionSwizzle/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  TestAppVersionSwizzle
+//
+//  Created by Brook_Mobius on 5/22/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
