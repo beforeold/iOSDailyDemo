1d259dc9e5aca5dff104ea62a81e2071356f8c0a
beforeold <ibeforeold@gmail.com>
2023-05-24 18:28:04 +0800

    add TestCustomAppStorage

---
+++ sources/TestCustomAppStorage/TestCustomAppStorage/TestCustomAppStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCustomAppStorageApp.swift
+//  TestCustomAppStorage
+//
+//  Created by Brook_Mobius on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct TestCustomAppStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
