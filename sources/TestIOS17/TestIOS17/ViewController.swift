b49993e547e3bc07df2d130fe20fd2871a4212bc
beforeold <ibeforeold@gmail.com>
2023-06-15 17:33:10 +0800

    add testSensitiveContentAnalysis

--- sources/TestIOS17/TestIOS17/ViewController.swift
+++ sources/TestIOS17/TestIOS17/ViewController.swift
@@ -12,8 +12,12 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    
+    viewIsAppearing(true)
+  }
+  
+  func testSensitiveContentAnalysis() {
+    // import SensitiveContentAnalysis
   }
 
-
 }
-
