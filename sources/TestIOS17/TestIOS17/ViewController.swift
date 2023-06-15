34e33eefb998d7d2476113da5649556884bc8af7
beforeold <ibeforeold@gmail.com>
2023-06-15 16:33:44 +0800

    add TestIOS17

---
+++ sources/TestIOS17/TestIOS17/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestIOS17
+//
+//  Created by Brook_Mobius on 6/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
