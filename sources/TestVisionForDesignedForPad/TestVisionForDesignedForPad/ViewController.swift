5488c5c6d59d6654592ae2a4c31f9714c320135d
beforeold <ibeforeold@gmail.com>
2023-07-12 15:57:56 +0800

    add TestVisionForDesignedForPad

---
+++ sources/TestVisionForDesignedForPad/TestVisionForDesignedForPad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestVisionForDesignedForPad
+//
+//  Created by Brook_Mobius on 7/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
