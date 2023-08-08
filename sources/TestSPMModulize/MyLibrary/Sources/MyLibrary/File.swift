6fa95cc253f20ba198a23ebdfe302fd12bcd2f67
beforeold <ibeforeold@gmail.com>
2023-08-08 11:22:50 +0800

    add file to TestSPMModulize

---
+++ sources/TestSPMModulize/MyLibrary/Sources/MyLibrary/File.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Brook_Mobius on 8/8/23.
+//
+
+import Foundation
+
+public struct File {
+  public var name: String
+  
+  public init(name: String) {
+    self.name = name
+  }
+}
