a93e97858572397de12a24ab423b7aff468c083f
beforeold <ibeforeold@gmail.com>
2023-09-14 18:39:37 +0800

    feat: add ImageChecker

---
+++ sources/ImageChecker/ImageChecker/ImageCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCheckerApp.swift
+//  ImageChecker
+//
+//  Created by Brook_Mobius on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
