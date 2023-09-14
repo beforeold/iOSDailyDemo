e536b0a9c79112795fbe23719091c30c7923a0db
beforeold <ibeforeold@gmail.com>
2023-09-15 03:52:58 +0800

    feat: add TestObservation

---
+++ sources/TestObservationBP/TestObservationBP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestObservationBP
+//
+//  Created by beforeold on 2023/9/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
