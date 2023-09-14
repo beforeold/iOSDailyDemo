c126f9f004cc8f08f0a2b7918c18617699630dc4
beforeold <ibeforeold@gmail.com>
2023-09-15 03:52:58 +0800

    feat: add TestObservation

---
+++ sources/TestObservationBP/TestObservationBP/TestObservationBPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestObservationBPApp.swift
+//  TestObservationBP
+//
+//  Created by beforeold on 2023/9/15.
+//
+
+import SwiftUI
+
+@main
+struct TestObservationBPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sources/objccn-book-swift-ui-source/3.Data-and-Binding/Calculator-Starter-Excercise/Calculator/ContentView.swift
@@ -0,0 +1,124 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Wang Wei on 2019/06/17.
+//  Copyright © 2019 OneV's Den. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+let scale = UIScreen.main.bounds.width / 414
+
+
+struct ContentView : View {
+  @State private var brain: CalculatorBrain = .left("0")
+  
+  var body: some View {
+    VStack(spacing: 12) {
+      Spacer()
+      Text(brain.output)
+        .font(.system(size: 76))
+        .minimumScaleFactor(0.5)
+        .padding(.trailing, 24 * scale)
+        .frame(
+          minWidth: 0,
+          maxWidth: .infinity,
+          alignment: .trailing)
+      Button("Test") {
+        self.brain = .left("1.23")
+      }
+      CalculatorButtonPad(brain: $brain)
+        .padding(.bottom)
+    }
+  }
+}
+
+struct ContentView_Previews : PreviewProvider {
+  static var previews: some View {
+    ContentView()
+      .preferredColorScheme(.dark)
+  }
+}
+
+struct CalculatorButton : View {
+  
+  let fontSize: CGFloat = 38
+  let title: String
+  let size: CGSize
+  let backgroundColorName: String
+  let foregroundColor: Color
+  let action: () -> Void
+  
+  var body: some View {
+    Button(action: action) {
+      Text(title)
+        .font(.system(size: fontSize * scale))
+        .foregroundColor(foregroundColor)
+        .frame(width: size.width * scale, height: size.height * scale)
+        .background(Color(backgroundColorName))
+        .cornerRadius(size.width * scale / 2)
+    }
+  }
+}
+
+struct CalculatorButtonRow : View {
+  let row: [CalculatorButtonItem]
+  @Binding var brain: CalculatorBrain
+  
+  var body: some View {
+    HStack {
+      ForEach(row, id: \.self) { item in
+        CalculatorButton(
+          title: item.title,
+          size: item.size,
+          backgroundColorName: item.backgroundColorName,
+          foregroundColor: item.foregroundColor)
+        {
+          print("Button: \(item.title)")
+          self.brain = Reducer.reduce(state: self.brain,
+                                      action: item)
+        }
+      }
+    }
+  }
+}
+
+struct CalculatorButtonPad: View {
+  @Binding var brain: CalculatorBrain
+  let pad: [[CalculatorButtonItem]] = [
+    [.command(.clear), .command(.flip),
+     .command(.percent), .op(.divide)],
+    [.digit(7), .digit(8), .digit(9), .op(.multiply)],
+    [.digit(4), .digit(5), .digit(6), .op(.minus)],
+    [.digit(1), .digit(2), .digit(3), .op(.plus)],
+    [.digit(0), .dot, .op(.equal)]
+  ]
+  
+  var body: some View {
+    VStack(spacing: 8) {
+      ForEach(pad, id: \.self) { row in
+        CalculatorButtonRow(row: row, brain: self._brain)
+      }
+    }
+  }
+}
+
+
+
+@propertyWrapper
+struct Nicer {
+  init(wrappedValue initialValue: Int) {
+    self.wrappedValue = initialValue
+  }
+  var wrappedValue: Int {
+    set {
+      
+    }
+    
+    get {
+      5
+    }
+  }
+}
