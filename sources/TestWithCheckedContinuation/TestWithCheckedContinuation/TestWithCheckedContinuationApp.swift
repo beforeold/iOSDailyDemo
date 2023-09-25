e5c73cb1210fe24f9f46cc29d939520e0bd652c2
beforeold <ibeforeold@gmail.com>
2023-09-25 10:10:02 +0800

    feat: add TestTrimLines; add TestWithCheckedContinuation

---
+++ sources/TestWithCheckedContinuation/TestWithCheckedContinuation/TestWithCheckedContinuationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestWithCheckedContinuationApp.swift
+//  TestWithCheckedContinuation
+//
+//  Created by Brook_Mobius on 9/21/23.
+//
+
+import SwiftUI
+
+@main
+struct TestWithCheckedContinuationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
