f9cfbf8221b5cac3f71c4d7caba308b8bb4f0d1f
beforeold <ibeforeold@gmail.com>
2023-10-11 12:11:43 +0800

    feat: add TestSwiftUIGen

---
+++ sources/TestSwiftUIGen/TestSwiftUIGen/TestSwiftUIGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUIGenApp.swift
+//  TestSwiftUIGen
+//
+//  Created by Brook_Mobius on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUIGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
