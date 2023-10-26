320d13d0a644a455c3d52bfc7148848baee74a71
beforeold <ibeforeold@gmail.com>
2023-10-26 19:55:19 +0800

    feat: add TestStyleCollectionView

---
+++ sources/TestStyleCollectionView/TestStyleCollectionView/TestStyleCollectionViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestStyleCollectionViewApp.swift
+//  TestStyleCollectionView
+//
+//  Created by Brook_Mobius on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct TestStyleCollectionViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
