28a166bc9fb682b027004746e368795618ce1479
beforeold <ibeforeold@gmail.com>
2023-11-01 00:18:56 +0800

    feat: add TesrMultiplatform20231031

--- sources/TestVisionForDesignedForPad/TestVisionForDesignedForPad/ViewController.swift
+++ sources/TestVisionForDesignedForPad/TestVisionForDesignedForPad/ViewController.swift
@@ -13,7 +13,12 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
-
-
+  
+  func foo() {
+#if os(xrOS)
+    print("yo xi")
+#endif
+  }
+  
 }
 
