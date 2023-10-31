59e19406933a0fb6352fd82a0b995fe47caaafb8
beforeold <ibeforeold@gmail.com>
2023-11-01 00:18:56 +0800

    feat: add TesrMultiplatform20231031

---
+++ sources/TesrMultiplatform20231031/TesrMultiplatform20231031/TesrMultiplatform20231031App.swift
@@ -0,0 +1,17 @@
+//
+//  TesrMultiplatform20231031App.swift
+//  TesrMultiplatform20231031
+//
+//  Created by beforeold on 2023/10/31.
+//
+
+import SwiftUI
+
+@main
+struct TesrMultiplatform20231031App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
