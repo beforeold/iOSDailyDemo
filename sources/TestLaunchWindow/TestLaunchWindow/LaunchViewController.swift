efd6d60cbbd428c509d30bd6ffb68b0e803a8074
beforeold <ibeforeold@gmail.com>
2023-11-01 10:41:08 +0800

    feat: add TestLaunchWindow

---
+++ sources/TestLaunchWindow/TestLaunchWindow/LaunchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchViewController.swift
+//  TestLaunchWindow
+//
+//  Created by Brook_Mobius on 11/1/23.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    view.backgroundColor = .green
+
+    print(#function, self)
+  }
+
+}
---
+++ sources/TestLaunchWindow/TestLaunchWindow/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TestLaunchWindow
+//
+//  Created by Brook_Mobius on 11/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    view.backgroundColor = .gray
+
+    print(#function, self)
+  }
+
+
+}
+
