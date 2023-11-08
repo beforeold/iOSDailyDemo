f3a4df67df1bf7ae7caf4406ad2948271cd3f5b3
beforeold <ibeforeold@gmail.com>
2023-11-08 17:43:40 +0800

    feat: update TestWindowRootViewControlelrTransition

--- sources/TestWindowRootViewControlelrTransition/TestWindowRootViewControlelrTransition/ViewController.swift
+++ sources/TestWindowRootViewControlelrTransition/TestWindowRootViewControlelrTransition/ViewController.swift
@@ -7,11 +7,31 @@
 
 import UIKit
 
-class YourNewViewController: UIViewController {
+class PresentedViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
     view.backgroundColor = .red
+
+    let button = UIButton(frame: .init(x: 100, y: 100, width: 200, height: 100))
+    button.backgroundColor = .blue
+    button.setTitleColor(.white, for: .normal)
+    button.addTarget(self, action: #selector(onTap), for: .touchUpInside)
+    view.addSubview(button)
+  }
+
+  @objc func onTap() {
+    print(#function, self)
+
+    dismiss(animated: true)
+  }
+}
+
+class NewViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    view.backgroundColor = .purple
   }
 }
 
@@ -21,10 +41,28 @@ class ViewController: UIViewController {
     super.viewDidLoad()
 
     view.backgroundColor = .green
+
+    let button = UIButton(frame: .init(x: 100, y: 100, width: 200, height: 100))
+    button.backgroundColor = .blue
+    button.setTitleColor(.white, for: .normal)
+    button.addTarget(self, action: #selector(onTap), for: .touchUpInside)
+    view.addSubview(button)
   }
 
-  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    let newRootViewController = YourNewViewController()
+  @objc func onTap() {
+    print(#function, self)
+
+    let presented = PresentedViewController()
+    presented.modalPresentationStyle = .fullScreen
+    present(presented, animated: true) {
+      let window = UIApplication.shared.windows.first { $0.isKeyWindow }
+      window?.rootViewController = NewViewController()
+    }
+  }
+
+  /// 测试重新设置 root vc
+  func testResetRoot() {
+    let newRootViewController = PresentedViewController()
     let window = UIApplication.shared.windows.first { $0.isKeyWindow }
 
     if let window = window {
