d0b4df53dc43f5eef69ced102ec030ed918314da
beforeold <ibeforeold@gmail.com>
2023-11-09 17:39:26 +0800

    feat: add TestEnvironment

---
+++ sources/TestEnvironment/TestEnvironment/TestEnvironmentApp.swift
@@ -0,0 +1,18 @@
+//
+//  TestEnvironmentApp.swift
+//  TestEnvironment
+//
+//  Created by Brook_Mobius on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TestEnvironmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            // ContentView()
+          Text("Hello")
+        }
+    }
+}
