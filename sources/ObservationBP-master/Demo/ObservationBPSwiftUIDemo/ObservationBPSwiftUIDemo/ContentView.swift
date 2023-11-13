a6718beb617f4ff3d7383fefc2edc0a7efb8672b
beforeold <ibeforeold@gmail.com>
2023-11-13 20:00:55 +0800

    feat: update ObservationBPSwiftUIDemo

--- sources/ObservationBP-master/Demo/ObservationBPSwiftUIDemo/ObservationBPSwiftUIDemo/ContentView.swift
+++ sources/ObservationBP-master/Demo/ObservationBPSwiftUIDemo/ObservationBPSwiftUIDemo/ContentView.swift
@@ -9,51 +9,69 @@ import SwiftUI
 import ObservationBP
 
 @Observable final class Person {
-    var name: String
-    var age: Int
-    
-    init(name: String, age: Int) {
-        self.name = name
-        self.age = age
-    }
+  var name: String
+  var age: Int
+
+  var isReady: Bool = false
+  var readyCount: Int = 0
+
+  init(name: String, age: Int) {
+    self.name = name
+    self.age = age
+  }
 }
 
 struct ContentView: View {
-    
-    private var person = Person(name: "Tom", age: 12)
-    
-    var body: some View {
-        ObservationView {
-            VStack {
-                Text(person.name)
-                Text("\(person.age)")
-                HStack {
-                    Button("+") { person.age += 1 }
-                    Button("-") { person.age -= 1 }
-                }
-            }
-            .padding()
+
+  private var person = Person(name: "Tom", age: 12)
+
+  var body: some View {
+    ObservationView {
+      VStack {
+        Text(person.name)
+        Text("\(person.age)")
+
+        if person.isReady {
+          Button("To Not Ready") {
+            person.isReady = false
+            person.readyCount += 1
+          }
+          Text(person.readyCount.description)
+        } else {
+          Button("To Ready") {
+            person.isReady = true
+            person.readyCount += 1
+          }
+          Text(person.readyCount.description)
         }
-    }
-}
 
-#Preview {
-    ContentView()
-}
-
-
-@Observable class Object {
-  var name = "beforeold"
-}
-
-private struct TestView: ObserableView {
-  let object = Object()
-  
-  var observerableBody: some View {
-    Text("Hello \(object.name)")
-    
-    Button("rename") {
-      object.name = "other name"
+        HStack {
+          Button("+") { person.age += 1 }
+          Button("-") { person.age -= 1 }
+        }
+      }
+      .padding()
     }
   }
 }
+
+#Preview {
+  ContentView()
+}
+
+//
+//@Observable class Object {
+//  var name = "beforeold"
+//}
+//
+//private struct TestView: ViewBP {
+//  let object = Object()
+//
+//  var bodyBP: some View {
+//    Text("Hello \(object.name)")
+//
+//    Button("rename") {
+//      object.name = "other name"
+//    }
+//  }
+//}
