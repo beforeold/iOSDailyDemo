bd38beee6bb2b6a9865905d66cd736bb0aaf064f
beforeold <ibeforeold@gmail.com>
2023-11-19 17:28:10 +0800

    update ObservationBPSwiftUIDemo

---
+++ sources/ObservationBP-windpan/Demo/ObservationBPSwiftUIDemo/ObservationBPSwiftUIDemo/ObservationView.swift
@@ -0,0 +1,43 @@
+//
+//  ObservationView.swift
+//  ObservationBPSwiftUIDemo
+//
+//  Created by Brook_Mobius on 11/19/23.
+//
+
+import ObservationBP
+import SwiftUI
+
+public struct ObservationView<Content: View>: View {
+
+  @State private var token: Int = 0
+
+  private let content: () -> Content
+  public init(
+    @ViewBuilder _ content: @escaping () -> Content
+  ) {
+    self.content = content
+  }
+  
+  public var body: some View {
+    _ = token
+    return withObservationTracking {
+      content()
+    } onChange: {
+      token += 1
+    }
+  }
+}
+
+protocol ViewBP: View {
+  associatedtype BodyBP: View
+  @ViewBuilder var bodyBP: BodyBP { get }
+}
+
+extension ViewBP {
+  var body: some View {
+    ObservationView {
+      bodyBP
+    }
+  }
+}
