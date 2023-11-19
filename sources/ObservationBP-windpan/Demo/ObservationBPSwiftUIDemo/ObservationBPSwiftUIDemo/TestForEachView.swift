f0335fcdeafb742b5a5ad89362ea3a80644f9e2e
beforeold <ibeforeold@gmail.com>
2023-11-19 17:30:25 +0800

    feat: update TestForEachView

--- sources/ObservationBP-windpan/Demo/ObservationBPSwiftUIDemo/ObservationBPSwiftUIDemo/TestForEachView.swift
+++ sources/ObservationBP-windpan/Demo/ObservationBPSwiftUIDemo/ObservationBPSwiftUIDemo/TestForEachView.swift
@@ -8,10 +8,30 @@
 import SwiftUI
 import ObservationBP
 
+@Observable
+class ForEeachModel {
+  var titles: [String] = [
+    "first",
+    "second",
+    "third",
+  ]
+
+  var selectedTitle: String? = nil
+}
+
 struct TestForEachView: View {
+  let model = ForEeachModel()
+
   var body: some View {
     VStack {
-
+      ForEach(model.titles, id: \.self) { title in
+        HStack {
+          Text(title)
+          if model.selectedTitle == title {
+            Text("isSelected")
+          }
+        }
+      }
     }
   }
 }
