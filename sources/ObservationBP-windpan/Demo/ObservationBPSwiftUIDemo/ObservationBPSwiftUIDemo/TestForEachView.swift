be6209c0966a83f0b2037139d393cdebfc5da39a
beforeold <ibeforeold@gmail.com>
2023-11-19 17:28:10 +0800

    update ObservationBPSwiftUIDemo

---
+++ sources/ObservationBP-windpan/Demo/ObservationBPSwiftUIDemo/ObservationBPSwiftUIDemo/TestForEachView.swift
@@ -0,0 +1,21 @@
+//
+//  TestForEachView.swift
+//  ObservationBPSwiftUIDemo
+//
+//  Created by Brook_Mobius on 11/19/23.
+//
+
+import SwiftUI
+import ObservationBP
+
+struct TestForEachView: View {
+  var body: some View {
+    VStack {
+
+    }
+  }
+}
+
+#Preview {
+  TestForEachView()
+}
