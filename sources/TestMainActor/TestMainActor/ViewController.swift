729a10fa65905266a9d3e93de4195b17d47c83a6
beforeold <ibeforeold@gmail.com>
2024-01-03 19:57:30 +0800

    feat: add TestMainActor

---
+++ sources/TestMainActor/TestMainActor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestMainActor
+//
+//  Created by Brook_Mobius on 1/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
