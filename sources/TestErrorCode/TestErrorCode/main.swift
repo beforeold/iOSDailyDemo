a6bf76eff2510b0d4513deeaedcc151f8e01152f
beforeold <ibeforeold@gmail.com>
2024-03-05 10:21:34 +0800

    update TestErrorCode

--- sources/TestErrorCode/TestErrorCode/main.swift
+++ sources/TestErrorCode/TestErrorCode/main.swift
@@ -9,12 +9,21 @@ import Foundation
 
 enum Failure: Error {
   case placeholder(Int)
+  case other(Int)
 }
 
-let nsError = Failure.placeholder(5) as NSError
-let code = nsError.code
-let domain = nsError.domain
-let info = nsError.userInfo
-print(nsError, code, domain, info)
+do {
+  let nsError = Failure.placeholder(5) as NSError
+  let code = nsError.code
+  let domain = nsError.domain
+  let info = nsError.userInfo
+  print(nsError, code, domain, info)
+}
 
-
+do {
+  let nsError = Failure.other(5) as NSError
+  let code = nsError.code
+  let domain = nsError.domain
+  let info = nsError.userInfo
+  print(nsError, code, domain, info)
+}
