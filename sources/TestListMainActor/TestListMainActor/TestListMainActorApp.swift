7407f4c4453d619bb0119f5f0555d02d23db3800
xipingping <xipingping@ios-builddeMac-Studio.local>
2024-04-23 15:54:14 +0800

    add TestListMainActor

---
+++ sources/TestListMainActor/TestListMainActor/TestListMainActorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestListMainActorApp.swift
+//  TestListMainActor
+//
+//  Created by xipingping on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct TestListMainActorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
