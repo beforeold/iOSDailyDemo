537a3f3ad076fdeaa406a6120309680c1f5f57cd
xipingping <xipingping@ios-builddeMac-Studio.local>
2024-04-24 17:01:46 +0800

    add TestLazyVStack

---
+++ sources/TestLazyVStack/TestLazyVStack/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  TestLazyVStack
+//
+//  Created by xipingping on 4/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    ScrollView {
+      LazyVStack(alignment: .leading) {
+        ForEach(1...100, id: \.self) { value in
+          Text("Row \(value)")
+            .frame(height: 200)
+            .onAppear {
+              print("appear", value)
+            }
+            .onDisappear {
+              print("disappear", value)
+            }
+        }
+      }
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ sources/TestLazyVStack/TestLazyVStack/TestLazyVStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestLazyVStackApp.swift
+//  TestLazyVStack
+//
+//  Created by xipingping on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct TestLazyVStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
