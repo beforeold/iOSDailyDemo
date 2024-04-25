0ac938c7c9e913cbf8fa3881eaf07513cbd50969
xipingping <xipingping@ios-builddeMac-Studio.local>
2024-04-25 11:49:28 +0800

    feat: update TestLazyVStack

--- sources/TestLazyVStack/TestLazyVStack/ContentView.swift
+++ sources/TestLazyVStack/TestLazyVStack/ContentView.swift
@@ -11,19 +11,69 @@ struct ContentView: View {
   var body: some View {
     ScrollView {
       LazyVStack(alignment: .leading) {
-        ForEach(0...100, id: \.self) { value in
-          Text("Row \(value)")
-            .frame(height: 200)
-            .onAppear {
-              print("appear", value)
-            }
-            .onDisappear {
-              print("disappear", value)
-            }
-        }
+
+        manualBuild()
+
       }
     }
     .padding()
+  }
+
+  @ViewBuilder
+  private func forEachBuild() -> some View {
+    ForEach(0...100, id: \.self) { value in
+      Text("Row \(value)")
+        .frame(height: 200)
+        .onAppear {
+          print("appear", value)
+        }
+        .onDisappear {
+          print("disappear", value)
+        }
+    }
+  }
+
+  @ViewBuilder
+  private func manualBuild() -> some View {
+    var value = 0
+
+    makeText(&value)
+
+    makeText(&value)
+
+    makeText(&value)
+
+    makeText(&value)
+
+    makeText(&value)
+
+    makeText(&value)
+
+    makeText(&value)
+
+    makeText(&value)
+
+    makeText(&value)
+
+    makeText(&value)
+
+    makeText(&value)
+
+    makeText(&value)
+  }
+
+  @ViewBuilder
+  private func makeText(_ value: inout Int) -> some View {
+    let _ = { value += 1 }()
+
+    Text("Row \(value)")
+      .frame(height: 200)
+      .onAppear { [value] in
+        print("appear", value)
+      }
+      .onDisappear { [value] in
+        print("disappear", value)
+      }
   }
 }
 
