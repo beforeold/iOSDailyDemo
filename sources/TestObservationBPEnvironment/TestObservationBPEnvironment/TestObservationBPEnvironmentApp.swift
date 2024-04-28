8b69101b1f8b1ca22730b5a88e86c2fdf10f6254
xipingping <xipingping@ios-builddeMac-Studio.local>
2024-04-28 17:40:59 +0800

    add TestObservationBPEnvironment

---
+++ sources/TestObservationBPEnvironment/TestObservationBPEnvironment/TestObservationBPEnvironmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestObservationBPEnvironmentApp.swift
+//  TestObservationBPEnvironment
+//
+//  Created by xipingping on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TestObservationBPEnvironmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
