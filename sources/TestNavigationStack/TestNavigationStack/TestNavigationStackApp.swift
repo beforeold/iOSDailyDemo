1e1a5b432db619144f8b7c417c6e66374e128b72
beforeold <ibeforeold@gmail.com>
2024-04-29 09:02:59 +0800

    add TestNavigationStack

---
+++ sources/TestNavigationStack/TestNavigationStack/TestNavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNavigationStackApp.swift
+//  TestNavigationStack
+//
+//  Created by beforeold on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct TestNavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
