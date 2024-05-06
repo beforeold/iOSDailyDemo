81c4f1fb8346002dfd2f150fb30af4e8e80b2b97
xipingping <xipingping@ios-builddeMac-Studio.local>
2024-05-06 15:09:07 +0800

    add TestSwiftUILongPress

---
+++ sources/TestSwiftUILongPress/TestSwiftUILongPress/TestSwiftUILongPressApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUILongPressApp.swift
+//  TestSwiftUILongPress
+//
+//  Created by xipingping on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUILongPressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
