81f6f572894f6206ea3e9abda8e9b34004b4becf
xipingping <xipingping@ios-builddeMac-Studio.local>
2024-05-13 09:27:23 +0800

    add TestScopedFunction; add TestLottieSPM

---
+++ sources/TestLottieSPM/TestLottieSPM/TestLottieSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestLottieSPMApp.swift
+//  TestLottieSPM
+//
+//  Created by xipingping on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TestLottieSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
