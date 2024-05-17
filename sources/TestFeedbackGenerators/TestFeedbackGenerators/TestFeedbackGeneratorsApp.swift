5e0cf970672c1aa8eafec934012f3bfe50b9b4fb
xipingping <xipingping@ios-builddeMac-Studio.local>
2024-05-17 14:39:07 +0800

    add TestFeedbackGenerators

---
+++ sources/TestFeedbackGenerators/TestFeedbackGenerators/TestFeedbackGeneratorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestFeedbackGeneratorsApp.swift
+//  TestFeedbackGenerators
+//
+//  Created by xipingping on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct TestFeedbackGeneratorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
