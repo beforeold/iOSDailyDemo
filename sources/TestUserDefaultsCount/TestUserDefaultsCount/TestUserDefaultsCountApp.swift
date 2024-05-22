ac1d009e04dc702205934ad037fc43cca4e14f8f
beforeold <ibeforeold@gmail.com>
2024-05-22 10:58:23 +0800

    add TestUIKitContextMenu

---
+++ sources/TestUserDefaultsCount/TestUserDefaultsCount/TestUserDefaultsCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestUserDefaultsCountApp.swift
+//  TestUserDefaultsCount
+//
+//  Created by xipingping on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct TestUserDefaultsCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
