dc9aaf8b6fab4c0783e674bcadd403bb419ad2d7
beforeold <ibeforeold@gmail.com>
2024-05-23 12:03:19 +0800

    add TestBindable

---
+++ sources/TestBindable/TestBindable/TestBindableApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBindableApp.swift
+//  TestBindable
+//
+//  Created by xipingping on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct TestBindableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
