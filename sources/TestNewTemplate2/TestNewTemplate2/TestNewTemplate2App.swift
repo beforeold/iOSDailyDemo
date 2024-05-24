20bbfecd6fcc39fe50d0506f577b98466fdb9379
beforeold <ibeforeold@gmail.com>
2024-05-24 11:30:02 +0800

    add newtemplate2

---
+++ sources/TestNewTemplate2/TestNewTemplate2/TestNewTemplate2App.swift
@@ -0,0 +1,17 @@
+//
+//  TestNewTemplate2App.swift
+//  TestNewTemplate2
+//
+//  Created by xipingping on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct TestNewTemplate2App: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
