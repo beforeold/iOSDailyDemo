44e0136e4dd1c594afecec909b62a590aa02ba4c
beforeold <ibeforeold@gmail.com>
2024-05-24 11:08:27 +0800

    feat: add TestNewTemplate

---
+++ sources/TestNewTemplate/TestNewTemplate/TestNewTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNewTemplateApp.swift
+//  TestNewTemplate
+//
+//  Created by xipingping on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct TestNewTemplateApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
