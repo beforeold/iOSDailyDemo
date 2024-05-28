dd1bf7913cd485a1a3bd1175e8b4fa1179bc8885
beforeold <ibeforeold@gmail.com>
2024-05-28 17:16:54 +0800

    add TestIP

---
+++ sources/TestIP/TestIP/TestIPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestIPApp.swift
+//  TestIP
+//
+//  Created by xipingping on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TestIPApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
