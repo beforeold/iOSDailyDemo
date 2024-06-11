21e3005bfa52bc992164881335ac4e4ee250030a
beforeold <ibeforeold@gmail.com>
2024-06-11 10:21:53 +0800

    add TestTextEditorSelectionOnly

---
+++ sources/TestTextEditorSelectionOnly/TestTextEditorSelectionOnly/TestTextEditorSelectionOnlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTextEditorSelectionOnlyApp.swift
+//  TestTextEditorSelectionOnly
+//
+//  Created by xipingping on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TestTextEditorSelectionOnlyApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
