8bd753aa748541b65be72125a4391b69edbec5e7
beforeold <ibeforeold@gmail.com>
2024-06-13 11:28:45 +0800

    update TestDictionaryMerging

--- sources/TestDictionaryMerging/TestDictionaryMerging/main.swift
+++ sources/TestDictionaryMerging/TestDictionaryMerging/main.swift
@@ -7,9 +7,11 @@
 
 import Foundation
 
-protocol AnyProperty {
+//protocol AnyProperty {
+//
+//}
 
-}
+typealias AnyProperty = Any
 
 // extension Int conforms to AnyProperty
 extension Int: AnyProperty {
@@ -24,4 +26,12 @@ var properties: [String: AnyProperty] = [
 let propertiesAfter = commonProperties.merging(properties) { _, _ in
 }
 
-print("ret", propertiesAfter)
+let nsDictionay = NSDictionary(dictionary: propertiesAfter)
+
+print("merging ret", propertiesAfter)
+print("as NSDictionary", nsDictionay)
+
+// __SwiftValue
+let key1 = nsDictionay["key1"] as! NSObject
+print("ns key1:", key1)
+print("ns key1 type:", type(of: key1))
