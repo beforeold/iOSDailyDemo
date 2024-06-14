8975a7c727548f09264139c98da2407165bb13c0
beforeold <ibeforeold@gmail.com>
2024-06-14 09:54:59 +0800

    add TestStoreKitReceipts

---
+++ sources/TestStoreKitReceipts/TestStoreKitReceipts/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  TestStoreKitReceipts
+//
+//  Created by xipingping on 6/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    DebugReceiptsView()
+  }
+}
+
+#Preview {
+  ContentView()
+    .preferredColorScheme(.dark)
+}
