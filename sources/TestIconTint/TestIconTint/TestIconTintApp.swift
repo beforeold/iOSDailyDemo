0890a260ef99b3bf9de429486556a9cfa78b851e
beforeold <ibeforeold@gmail.com>
2024-06-25 10:04:01 +0800

    add TestIconTint

---
+++ sources/TestIconTint/TestIconTint/TestIconTintApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestIconTintApp.swift
+//  TestIconTint
+//
+//  Created by xipingping on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct TestIconTintApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
