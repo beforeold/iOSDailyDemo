983ee270dfead462fdc4bcf2c0c1c49fc62171f1
beforeold <ibeforeold@gmail.com>
2024-07-01 21:54:28 +0800

    add TestMenuState

---
+++ sources/TestMenuState/TestMenuState/TestMenuStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMenuStateApp.swift
+//  TestMenuState
+//
+//  Created by beforeold on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TestMenuStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
