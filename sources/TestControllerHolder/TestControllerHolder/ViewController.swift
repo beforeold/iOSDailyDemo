a9b89e472f1d3c200e5a0e9eef757a64b00e24bd
beforeold <ibeforeold@gmail.com>
2024-07-04 15:41:50 +0800

    add View+ControllerHolder

---
+++ sources/TestControllerHolder/TestControllerHolder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestControllerHolder
+//
+//  Created by xipingping on 7/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
