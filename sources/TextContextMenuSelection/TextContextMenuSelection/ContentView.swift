3c44cc29bab0f6421bf8cf38557f452268b42b92
beforeold <ibeforeold@gmail.com>
2024-07-07 21:51:22 +0800

    add ObservedView

--- sources/TextContextMenuSelection/TextContextMenuSelection/ContentView.swift
+++ sources/TextContextMenuSelection/TextContextMenuSelection/ContentView.swift
@@ -8,41 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-      ContextMenuItemExample()
-    }
+  var body: some View {
+    ContextMenuItemExample(
+      items: [
+        .init(name: "1"),
+        .init(name: "2"),
+        .init(name: "3"),
+        .init(name: "4"),
+      ]
+    )
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
 
 struct ContextMenuItemExample: View {
-    var items: [Item]
-    @State private var selection = Set<Item.ID>()
+  struct Item: Identifiable {
+    let id = UUID()
+    var name: String
+  }
+  var items: [Item]
+
+  @State private var selection = Set<Item.ID>()
 
 
-    var body: some View {
-        List(selection: $selection) {
-            ForEach(items) { item in
-                Text(item.name)
-            }
+  var body: some View {
+    NavigationStack {
+      List(selection: $selection) {
+        ForEach(items) { item in
+          HStack {
+            Image(systemName: "clock")
+            Text(item.name)
+          }
+          .frame(height: 80)
         }
-        .contextMenu(forSelectionType: Item.ID.self) { items in
-            if items.isEmpty { // Empty area menu.
-                Button("New Item") { }
-
-
-            } else if items.count == 1 { // Single item menu.
-                Button("Copy") { }
-                Button("Delete", role: .destructive) { }
-
-
-            } else { // Multi-item menu.
-                Button("Copy") { }
-                Button("New Folder With Selection") { }
-                Button("Delete Selected", role: .destructive) { }
-            }
-        }
+      }
+      .toolbar {
+        EditButton()
+      }
     }
+    .contextMenu(forSelectionType: Item.ID.self) { items in
+      if items.isEmpty { // Empty area menu.
+        Button("New Item") { }
+
+
+      } else if items.count == 1 { // Single item menu.
+        Button("Copy") { }
+        Button("Delete", role: .destructive) { }
+
+
+      } else { // Multi-item menu.
+        Button("Copy") { }
+        Button("New Folder With Selection") { }
+        Button("Delete Selected", role: .destructive) { }
+      }
+    }
+  }
 }
