8283014c1c04a9a2372b424e26629490edd52235
beforeold <ibeforeold@gmail.com>
2024-07-09 13:10:41 +0800

    add TestUIViewWrapperUpdate

---
+++ sources/TestUIViewWrapperUpdate/TestUIViewWrapperUpdate/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  TestUIViewWrapperUpdate
+//
+//  Created by xipingping on 7/9/24.
+//
+
+import SwiftUI
+
+struct UIViewWrapper: UIViewRepresentable {
+  var view: UIView
+  var color: UIColor
+
+  func makeUIView(context: Context) -> UIView {
+    print("makeUIView")
+
+    let view = self.view
+    view.backgroundColor = color
+
+    return view
+  }
+
+  func updateUIView(_ uiView: UIView, context: Context) {
+//    uiView.backgroundColor = color
+    print("update")
+  }
+}
+
+struct ContentView: View {
+  @State private var color: UIColor = .red
+
+  @State var id = UUID()
+
+  var body: some View {
+    VStack {
+      UIViewWrapper(view: .init(), color: color)
+        .onTapGesture {
+          print("on tap")
+          self.color = .blue
+
+
+//          self.id = UUID()
+        }
+    }
+    .padding()
+//    .id(id)
+  }
+}
+
+#Preview {
+  ContentView()
+    .preferredColorScheme(.dark)
+}
---
+++ sources/TestUIViewWrapperUpdate/TestUIViewWrapperUpdate/TestUIViewWrapperUpdateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestUIViewWrapperUpdateApp.swift
+//  TestUIViewWrapperUpdate
+//
+//  Created by xipingping on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TestUIViewWrapperUpdateApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
