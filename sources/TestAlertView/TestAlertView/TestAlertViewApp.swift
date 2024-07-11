bc97026433d7cdc242aadc4ff5f28f3f972cd689
beforeold <ibeforeold@gmail.com>
2024-07-11 12:00:22 +0800

    add SubscriptionAlertView

---
+++ sources/TestAlertView/TestAlertView/TestAlertViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAlertViewApp.swift
+//  TestAlertView
+//
+//  Created by xipingping on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TestAlertViewApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
