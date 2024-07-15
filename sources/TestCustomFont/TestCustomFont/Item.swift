a0f7c927a42f2b3d8bb36fe381a25cd84fb33ea1
beforeold <ibeforeold@gmail.com>
2024-07-15 17:26:39 +0800

    add TestCustomFont

---
+++ sources/TestCustomFont/TestCustomFont/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  TestCustomFont
+//
+//  Created by xipingping on 7/15/24.
+//
+
+import Foundation
+
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
