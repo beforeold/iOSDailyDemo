0951f8f2cd496511bcfa7068cd6ba8100991c307
beforeold <ibeforeold@gmail.com>
2024-09-18 20:07:54 +0800

    update TestSwiftUiIndicies

--- TestSwiftUiIndicies/TestSwiftUiIndicies/ContentViewIndexed.swift
+++ TestSwiftUiIndicies/TestSwiftUiIndicies/ContentViewIndexed.swift
@@ -1,10 +1,10 @@
+import Foundation
 import ObservationBP
 import SwiftUI
 
+struct ContentViewIndexed: View {
 
-public struct ContentView: View {
-
-  public enum Option: Hashable {
+  enum Option: Hashable {
     case first
     case second
     case third
@@ -31,7 +31,7 @@ public struct ContentView: View {
 
   var model: Model = .init()
 
-  public var body: some View {
+  var body: some View {
     ObservationView {
       let _ = print("bp 1")
 
@@ -42,19 +42,19 @@ public struct ContentView: View {
           model.append()
         }
 
-        ForEach(model.sections.indices, id: \.self) { item in
+        ForEach(model.sections.indiciesAndValues, id: \.index) { item in
           let _ = print("outer foreach \(item)")
-
-          let index = item
+          let index = item.index
           //        ObservationView {
           Section {
-            ForEach(model.sections[index].indices, id: \.self) { item in
+            ForEach(model.sections[index].indiciesAndValues, id: \.index) { item in
               let _ = print("inner foreach \(item)")
 
-              let index2 = item
+              let index2 = item.index
               Text("\(model.sections[index][index2])")
             }
           }
+
           //        }
         }
       }
@@ -63,5 +63,5 @@ public struct ContentView: View {
 }
 
 #Preview {
-  ContentView()
+  ContentViewIndexed()
 }
