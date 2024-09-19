e8ef08435f65fac352a8cc5387c25bc66e02e600
beforeold <ibeforeold@gmail.com>
2024-09-19 17:39:29 +0800

    complete TestPrivateSPM

--- sources/TestPrivateSPM/TestPrivateSPM/ContentView.swift
+++ sources/TestPrivateSPM/TestPrivateSPM/ContentView.swift
@@ -1,17 +1,26 @@
 import SwiftUI
 
+// import BizTemplate
+
+func foo() {
+  // File.hello()
+}
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
     }
+    .padding()
+    .onAppear {
+      foo()
+    }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
