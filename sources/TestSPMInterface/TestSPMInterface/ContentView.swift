ff89109e2131bb9233229272565266fee418f458
beforeold <ibeforeold@gmail.com>
2024-10-25 18:09:46 +0800

    add TestSPMInterface

---
+++ sources/TestSPMInterface/TestSPMInterface/ContentView.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+import TestSPMTargetsCore
+
+func foo() {
+  Core.foo()
+  Full.foo()
+}
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
