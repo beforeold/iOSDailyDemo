df7df880907191d333ae53d8213d8fed6cabfd1b
beforeold <ibeforeold@gmail.com>
2024-10-25 15:18:22 +0800

    add TestSPMTargets

---
+++ sources/TestSPMTargets/Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "TestSPMTargets",
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "TestSPMTargetsCore",
+      targets: ["TestSPMTargetsCore"]
+    ),
+    .library(
+      name: "TestSPMTargets",
+      targets: ["TestSPMTargets"]
+    ),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "TestSPMTargetsCore"
+    ),
+    .target(
+      name: "TestSPMTargets", dependencies: ["TestSPMTargetsCore"]
+    ),
+    .testTarget(
+      name: "TestSPMTargetsTests",
+      dependencies: ["TestSPMTargets"]
+    ),
+  ]
+)
