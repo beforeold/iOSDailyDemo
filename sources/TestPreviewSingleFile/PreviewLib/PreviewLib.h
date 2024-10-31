bb14da98946442b478c7dbe1849061c854a1db1a
beforeold <ibeforeold@gmail.com>
2024-10-31 23:39:16 +0800

    add TestPreviewSingleFile

---
+++ sources/TestPreviewSingleFile/PreviewLib/PreviewLib.h
@@ -0,0 +1,18 @@
+//
+//  PreviewLib.h
+//  PreviewLib
+//
+//  Created by Brook_Mobius on 10/31/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PreviewLib.
+FOUNDATION_EXPORT double PreviewLibVersionNumber;
+
+//! Project version string for PreviewLib.
+FOUNDATION_EXPORT const unsigned char PreviewLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PreviewLib/PublicHeader.h>
+
+
