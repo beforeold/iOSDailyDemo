e3310e8d7a4b7e23792666b91523458a68c3955e
beforeold <ibeforeold@gmail.com>
2024-11-04 16:27:01 +0800

    add TestOCAsObserableObject

---
+++ sources/TestOCAsObserableObject/TestOCAsObserableObject/ViewModel.h
@@ -0,0 +1,16 @@
+//
+//  ViewModel.h
+//  TestOCAsObserableObject
+//
+//  Created by xipingping on 11/4/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
