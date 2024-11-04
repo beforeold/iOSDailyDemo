04edc2b844ed9262a133de4b482cd3dbb79d6059
beforeold <ibeforeold@gmail.com>
2024-11-04 16:27:01 +0800

    add TestOCAsObserableObject

---
+++ sources/TestOCAsObserableObject/TestOCAsObserableObject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestOCAsObserableObject
+//
+//  Created by xipingping on 11/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
