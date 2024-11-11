8448728dd0731db0f6f49c30a0140fe6fc8d5b59
beforeold <ibeforeold@gmail.com>
2024-11-11 14:29:29 +0800

    add ObservableViewModel-master; add TestCopilotForXcode; add /MyPlayground2

---
+++ sources/TestCopilotForXcode/TestCopilotForXcode/TestCopilotForXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCopilotForXcodeApp.swift
+//  TestCopilotForXcode
+//
+//  Created by Brook_Mobius on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TestCopilotForXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
