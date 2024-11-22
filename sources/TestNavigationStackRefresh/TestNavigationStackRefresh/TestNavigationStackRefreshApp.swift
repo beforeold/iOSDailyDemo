34a7e88c743f384a587d4c646990861a33f8d11a
beforeold <ibeforeold@gmail.com>
2024-11-22 09:19:54 +0800

    update TestNavigationStackRefresh

--- sources/TestNavigationStackRefresh/TestNavigationStackRefresh/TestNavigationStackRefreshApp.swift
+++ sources/TestNavigationStackRefresh/TestNavigationStackRefresh/TestNavigationStackRefreshApp.swift
@@ -1,10 +1,32 @@
 import SwiftUI
 
+class Navigator {
+  var onPathChanged: ([String]) -> () = { _ in }
+  var path: [String] = [] {
+    didSet {
+      onPathChanged(path)
+    }
+  }
+
+  static let shared: Navigator = .init()
+
+}
+
 @main
 struct TestNavigationStackRefreshApp: App {
+  @State private var path: [String] = []
+
+  init() {
+    Navigator.shared.onPathChanged = { [_path] path in
+      print(path)
+      _path.wrappedValue = path
+      print(_path.wrappedValue)
+    }
+  }
+
   var body: some Scene {
     WindowGroup {
-      ContentView()
+      ContentView(path: $path)
     }
   }
 }
