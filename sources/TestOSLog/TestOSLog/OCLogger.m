b3a3602e3db85cb5e08d80334fbb06192570ec36
beforeold <ibeforeold@gmail.com>
2024-11-22 08:44:36 +0800

    add testinterface and testoslog

---
+++ sources/TestOSLog/TestOSLog/OCLogger.m
@@ -0,0 +1,11 @@
+#import "OCLogger.h"
+#import <OSLog/OSLog.h>
+
+@implementation OCLogger
+
+- (void)testLog {
+//  __auto_type log =
+  __auto_type logger = os_log(<#log#>, <#format, ...#>)
+}
+
+@end
