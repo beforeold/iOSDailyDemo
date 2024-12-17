3f9a9010548a2e2dc2664bb9b20ecf4bd4b3473a
beforeold <ibeforeold@gmail.com>
2024-12-17 09:11:55 +0800

    update TestClosureMainActor

--- sources/TestClosureMainActor/TestClosureMainActor/TestClosureMainActorApp.swift
+++ sources/TestClosureMainActor/TestClosureMainActor/TestClosureMainActorApp.swift
@@ -2,9 +2,9 @@ import SwiftUI
 
 @main
 struct TestClosureMainActorApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
