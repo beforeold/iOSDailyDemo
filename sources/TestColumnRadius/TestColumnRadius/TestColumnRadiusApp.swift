a534d3121b3186b0994a47c146d397afd31347c7
beforeold <ibeforeold@gmail.com>
2024-12-26 14:17:43 +0800

    add TestAppStorageInViewModel; add TestColumnRadius

---
+++ sources/TestColumnRadius/TestColumnRadius/TestColumnRadiusApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestColumnRadiusApp.swift
+//  TestColumnRadius
+//
+//  Created by Brook_Mobius on 12/25/24.
+//
+
+import SwiftUI
+
+@main
+struct TestColumnRadiusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
