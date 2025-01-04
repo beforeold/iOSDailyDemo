e68f5502bccd8da82b8a53d9bb5a445f03b8018a
beforeold <ibeforeold@gmail.com>
2025-01-04 11:58:25 +0800

    add TestStringInit

--- sources/TestNumberFomatter/TestNumberFomatter/main.swift
+++ sources/TestNumberFomatter/TestNumberFomatter/main.swift
@@ -3,11 +3,12 @@ import Foundation
 public extension NSNumber {
   func formatToBigNumber(locale: Locale = .current) -> String {
     let formatter = NumberFormatter()
-    formatter.locale = locale  // 指定为沙特阿拉伯地区
+     formatter.locale = locale  // 指定为沙特阿拉伯地区
 //    formatter.locale = Locale(identifier: "de_DE")  // 指定为德国
 //    formatter.locale = Locale(identifier: "en_US")  // 指定为美国
 
-    formatter.numberStyle = .currency
+    // formatter.numberStyle = .currency
+    formatter.numberStyle = .decimal
 //    formatter.maximumFractionDigits = 2 // Adjust the number of fraction digits as needed
 //    formatter.usesGroupingSeparator = true
 
@@ -19,7 +20,7 @@ public extension NSNumber {
   }
 }
 
-let number = 10000.005
+let number = 12345.67
 print(number, (number as NSNumber).formatToBigNumber(locale: Locale(identifier: "en_US")))
 print(number, (number as NSNumber).formatToBigNumber(locale: Locale(identifier: "de_DE")))
 print(number, (number as NSNumber).formatToBigNumber(locale: Locale(identifier: "ar_SA")))
