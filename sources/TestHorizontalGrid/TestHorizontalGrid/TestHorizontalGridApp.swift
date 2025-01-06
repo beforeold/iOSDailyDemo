82a330645ed39d2eabdec1a98d0b95364769c4f7
beforeold <ibeforeold@gmail.com>
2025-01-06 20:41:13 +0800

    add TestHorizontalGrid

---
+++ sources/TestHorizontalGrid/TestHorizontalGrid/TestHorizontalGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestHorizontalGridApp.swift
+//  TestHorizontalGrid
+//
+//  Created by Brook_Mobius on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct TestHorizontalGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
