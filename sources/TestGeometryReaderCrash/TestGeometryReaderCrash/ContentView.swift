9c6c048e03ecdb4e31aba351ad56c00b18a38058
beforeold <ibeforeold@gmail.com>
2025-01-19 12:07:09 +0800

    add TestGeometryReaderCrash

---
+++ sources/TestGeometryReaderCrash/TestGeometryReaderCrash/ContentView.swift
@@ -0,0 +1,52 @@
+import SwiftUI
+
+struct ContentView: View {
+  @State private var id = 0
+
+  var body: some View {
+    VStack {
+      Button("reset") {
+        id += 1
+      }
+
+      SubView()
+        .frame(width: 100, height: 100)
+        .background(.gray.opacity(0.3))
+        .id(id)
+
+      Spacer()
+    }
+    .padding()
+  }
+}
+
+struct SubView: View {
+  @State private var isLoading = true
+
+  var body: some View {
+    GeometryReader { geo in
+      DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+        let frame = geo.frame(in: .global).minY
+        print("delay", frame, isLoading)
+        isLoading = false
+      }
+
+      return VStack(spacing: 30) {
+        Group {
+          if isLoading {
+            ProgressView()
+          } else {
+
+          }
+        }
+        .frame(height: 40)
+
+        Color.blue
+      }
+    }
+  }
+}
+
+#Preview {
+  ContentView()
+}
