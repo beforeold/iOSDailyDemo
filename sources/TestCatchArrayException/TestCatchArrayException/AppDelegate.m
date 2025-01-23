05340887e4d1cc5ec0d8f39e09d3eeec5b31b7c6
beforeold <ibeforeold@gmail.com>
2025-01-23 10:21:00 +0800

    add TestCatchArrayException

---
+++ sources/TestCatchArrayException/TestCatchArrayException/AppDelegate.m
@@ -0,0 +1,42 @@
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+  // insert code here...
+  __auto_type array = [[[NSMutableArray alloc] init] copy];
+  @try {
+    NSNumber *element = [array objectAtIndex:666];
+    NSLog(@"element: %@", element);
+  } @catch (NSException *exception) {
+    NSLog(@"catched exception %@", exception.class);
+  }
+
+  return YES;
+}
+
+
+#pragma mark - UISceneSession lifecycle
+
+
+- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
+  // Called when a new scene session is being created.
+  // Use this method to select a configuration to create the new scene with.
+  return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+}
+
+
+- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
+  // Called when the user discards a scene session.
+  // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+  // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+}
+
+
+@end
---
+++ sources/TestCatchArrayException/TestCatchArrayException/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestCatchArrayException
+//
+//  Created by Brook_Mobius on 1/23/25.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  NSString * appDelegateClassName;
+  @autoreleasepool {
+      // Setup code that might create autoreleased objects goes here.
+      appDelegateClassName = NSStringFromClass([AppDelegate class]);
+  }
+  return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
