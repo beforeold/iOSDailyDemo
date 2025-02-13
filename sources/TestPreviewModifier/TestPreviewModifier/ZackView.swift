994c6b42885e14d42b4f3e544fb57fd324ab960d
beforeold <ibeforeold@gmail.com>
2025-02-13 17:43:18 +0800

    update TestPreviewModifier

---
+++ sources/TestPreviewModifier/TestPreviewModifier/ZackView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+struct ZackView: View {
+  var body: some View {
+    Text("ok i got it 123")
+  }
+}
+
+class Zack: ObservableObject {
+  init() {
+    print("init zack 2 3")
+  }
+}
+
+struct ZackModifier: PreviewModifier, Equatable {
+  static func makeSharedContext() async throws -> Zack {
+    print(#function)
+    try await Task.sleep(for: .seconds(2))
+    return Zack()
+  }
+
+  func body(content: Content, context: Zack) -> some View {
+    print(#function)
+    return content.environmentObject(context)
+  }
+
+}
+
+#Preview("z1", traits: .modifier(ZackModifier())) {
+  ZackView()
+}
+
+#Preview("z2", traits: .modifier(ZackModifier())) {
+  ZackView()
+}
