58c12842b2589432b56330a5efb8006d3d43f890
beforeold <ibeforeold@gmail.com>
2025-02-18 08:25:17 +0800

    update TestViewAsyncUpdate

--- sources/TestViewAsyncUpdate/TestViewAsyncUpdate/ContentView.swift
+++ sources/TestViewAsyncUpdate/TestViewAsyncUpdate/ContentView.swift
@@ -3,6 +3,8 @@ import SwiftUI
 struct ContentView: View {
   @State private var id = 0
   var body: some View {
+    let _ = print("content body")
+
     VStack {
       SubView {
         id += 1
@@ -15,12 +17,48 @@ struct ContentView: View {
 
 struct BackgroundView: View {
   class Object: NSObject {
+    static var increment = 0
+
+    var seq = 0
+
+    override init() {
+      super.init()
+
+      Object.increment += 1
+
+      self.seq = Object.increment
+      print("create", self.seq)
+    }
+
     deinit {
-      print("deinit")
+      print("deinit", self.seq)
     }
   }
 
-  @State private var object = Object()
+  class Object2: NSObject, ObservableObject {
+    static var increment = 0
+
+    var seq = 0
+
+    override init() {
+      super.init()
+
+      Object.increment += 1
+
+      self.seq = Object.increment
+      print("create", self.seq)
+    }
+
+    deinit {
+      print("deinit", self.seq)
+    }
+  }
+
+//  @State private var object = Object()
+
+  let object = Object()
+
+//  @StateObject private var object2 = Object2()
 
   var body: some View {
     Text("overlay")
@@ -36,8 +74,9 @@ struct SubView: View {
       action()
 
       Task {
+//        [value = _count] in
         try await Task.sleep(nanoseconds: 1_000_000_000)
-        count += 1
+//        value.wrappedValue += 1
         print("updated")
       }
     }
