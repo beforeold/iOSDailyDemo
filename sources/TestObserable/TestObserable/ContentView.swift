fbf5ce786742b7235a151e58a22097487d453c30
beforeold <ibeforeold@gmail.com>
2025-02-25 09:26:08 +0800

    add TestObserable

---
+++ sources/TestObserable/TestObserable/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestObserable
+//
+//  Created by Brook_Mobius on 2/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sources/TestObserable/TestObserable/TestObserableApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestObserableApp.swift
+//  TestObserable
+//
+//  Created by Brook_Mobius on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct TestObserableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
