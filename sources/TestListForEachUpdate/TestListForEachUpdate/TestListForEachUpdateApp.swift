4923803dc9c73128af8f31b0c36905cb1ea21d8c
beforeold <ibeforeold@gmail.com>
2025-02-28 15:17:25 +0800

    add TestListForEachUpdate

---
+++ sources/TestListForEachUpdate/TestListForEachUpdate/TestListForEachUpdateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestListForEachUpdateApp.swift
+//  TestListForEachUpdate
+//
+//  Created by Brook_Mobius on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct TestListForEachUpdateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
