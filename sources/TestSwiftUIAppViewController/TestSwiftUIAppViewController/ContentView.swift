7fc0ece63f492b04d343c757d8113ada48c192fa
beforeold <ibeforeold@gmail.com>
2025-03-04 16:36:21 +0800

    update page

--- sources/TestSwiftUIAppViewController/TestSwiftUIAppViewController/ContentView.swift
+++ sources/TestSwiftUIAppViewController/TestSwiftUIAppViewController/ContentView.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-class MyViewController: UIViewController {
+class FirstViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
@@ -10,23 +10,120 @@ class MyViewController: UIViewController {
   override func viewDidLayoutSubviews() {
     super.viewDidLayoutSubviews()
 
-    print(view.bounds, UIScreen.main.bounds)
+    print(self.view.bounds)
+  }
+
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+
+    print(self, #function)
   }
 }
 
-struct ContentView: UIViewControllerRepresentable {
+class SecondViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    view.backgroundColor = .red
+  }
+
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    print(self.view.bounds)
+  }
+
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+
+    print(self, #function)
+  }
+}
+
+class Model: ObservableObject {
+  @Published var selectedIndex = 0
+}
+
+struct ContentView: View {
+  @StateObject private var model = Model()
+
+  var body: some View {
+    ZStack(alignment: .bottom) {
+      HomeView(model: model)
+        .ignoresSafeArea()
+
+      Picker("", selection: $model.selectedIndex) {
+        Text("First").tag(0)
+        Text("Second").tag(1)
+      }
+      .pickerStyle(.segmented)
+    }
+  }
+}
+
+class MyPage: UIPageViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    navigationItem.trailingItemGroups = [.init(
+      barButtonItems: [.init(title: "Me", image: nil, target: nil, action: nil)],
+      representativeItem: nil
+    )]
+  }
+}
+
+struct HomeView: UIViewControllerRepresentable {
+  @ObservedObject var model: Model
+
+  class Coordinator: NSObject, UIPageViewControllerDelegate {
+    //    func pageViewController(
+    //      _ pageViewController: UIPageViewController,
+    //      spineLocationFor orientation: UIInterfaceOrientation
+    //    ) -> UIPageViewController.SpineLocation {
+    //      .mid
+    //    }
+
+    lazy var first: UIViewController = {
+      let vc = FirstViewController()
+
+      return vc
+    }()
+
+    lazy var second: UIViewController = {
+      let vc = SecondViewController()
+
+      return vc
+    }()
+  }
+
+  func makeCoordinator() -> Coordinator {
+    Coordinator()
+  }
+
   func makeUIViewController(context: Context) -> some UIViewController {
-    let vc = MyViewController()
-    let nav = UINavigationController(rootViewController: vc)
+    let page = MyPage(
+      transitionStyle: .scroll,
+      navigationOrientation: .horizontal
+    )
+    page.delegate = context.coordinator
+
+    let nav = UINavigationController(rootViewController: page)
+
     return nav
   }
 
   func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
-    print(uiViewController)
+    guard let page = uiViewController.children.first as? UIPageViewController else {
+      return
+    }
+
+    if model.selectedIndex == 0 {
+      page.setViewControllers([context.coordinator.first], direction: .reverse, animated: true)
+    } else {
+      page.setViewControllers([context.coordinator.second], direction: .forward, animated: true)
+    }
   }
 }
 
 #Preview {
   ContentView()
-    .ignoresSafeArea()
 }
