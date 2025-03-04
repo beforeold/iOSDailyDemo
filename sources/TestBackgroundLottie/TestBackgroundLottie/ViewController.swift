73dfb15d313ef07a2183ef4cc9ee9afba2d9e730
beforeold <ibeforeold@gmail.com>
2025-03-04 19:07:08 +0800

    add rotation for TestBackgroundLottie

--- sources/TestBackgroundLottie/TestBackgroundLottie/ViewController.swift
+++ sources/TestBackgroundLottie/TestBackgroundLottie/ViewController.swift
@@ -11,15 +11,28 @@ class ViewController: UIViewController {
     let animation = LottieAnimation.named("app_bg")
     let lottieView = LottieAnimationView(animation: animation)
     lottieView.loopMode = .loop
+    lottieView.contentMode = .scaleAspectFill
     lottieView.play()
 
     view.addSubview(lottieView)
-    lottieView.snp.makeConstraints { make in
-      make.edges.equalToSuperview()
-    }
-    lottieView.contentMode = .scaleAspectFill
 
-    let blurView = UIVisualEffectView(effect: UIBlurEffect(style: .systemThickMaterial))
+    let x = (UIScreen.main.bounds.height - UIScreen.main.bounds.width) * 0.5
+    lottieView.frame = CGRect(
+      x: -x,
+      y: x,
+      width: UIScreen.main.bounds.height,
+      height: UIScreen.main.bounds.width
+    )
+    lottieView.transform = .identity.rotated(by: .pi * 0.5)
+
+    let style: UIBlurEffect.Style? = .init(rawValue: 100)
+    print(style ?? "null style")
+
+    let blurView = UIVisualEffectView(
+      effect: UIBlurEffect(
+        style: .systemThickMaterial
+      )
+    )
     lottieView.addSubview(blurView)
     blurView.snp.makeConstraints { make in
       make.edges.equalToSuperview()
