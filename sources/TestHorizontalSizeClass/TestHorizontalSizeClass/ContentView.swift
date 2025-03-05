430e317496b5ef82a2056f44ba788794de089f2d
beforeold <ibeforeold@gmail.com>
2025-03-05 10:14:27 +0800

    add TestHorizontalSizeClass

---
+++ sources/TestHorizontalSizeClass/TestHorizontalSizeClass/ContentView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+struct ContentView: View {
+  @Environment(\.verticalSizeClass) private var horizontalSizeClass
+
+  var body: some View {
+    Text(horizontalSizeClass == .compact ? "Landscape" : "Portrait")
+      .onChange(of: horizontalSizeClass) { oldValue, newValue in
+        print("new value", newValue ?? "null", UIScreen.main.bounds)
+      }
+      .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+        print("new value", UIDevice.current.orientation.rawValue, UIScreen.main.bounds)
+      }
+  }
+}
+
+#Preview {
+  ContentView()
+}
