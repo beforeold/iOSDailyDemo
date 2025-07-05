76ec3032e77d1afdd209d6b625a7ca1bd50b56c9
beforeold <ibeforeold@gmail.com>
2025-07-05 11:23:47 +0800

    Add now playing artwork display in ContentView

--- sources/DemoNowPlaying/DemoNowPlaying/ContentView.swift
+++ sources/DemoNowPlaying/DemoNowPlaying/ContentView.swift
@@ -3,9 +3,15 @@ import SwiftUI
 
 struct ContentView: View {
   @State private var info = ""
+  @State private var artwork: UIImage? = nil
 
   var body: some View {
     VStack(spacing: 30) {
+      if let artwork = artwork {
+        Image(uiImage: artwork)
+          .resizable()
+          .frame(width: 100, height: 100)
+      }
       Text(info)
 
       Button("Get", action: getItem)
@@ -21,6 +27,7 @@ struct ContentView: View {
 
     guard let item = MPMusicPlayerController.systemMusicPlayer.nowPlayingItem else {
       info = "null music item"
+      artwork = nil
       return
     }
     info = ""
@@ -29,6 +36,9 @@ struct ContentView: View {
     info += "artist: \(item.artist ?? "null")\n"
     info += "title: \(item.title ?? "null")\n"
     info += "albumTitle: \(item.albumTitle ?? "null")\n"
+
+    let artworkImage = item.artwork?.image(at: CGSize(width: 100, height: 100))
+    artwork = artworkImage
   }
 }
 
