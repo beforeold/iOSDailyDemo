1d640d9cf1dfefa2365b3368a9be0d82028a37a3
beforeold <ibeforeold@gmail.com>
2025-09-02 14:13:59 +0800

    添加Mac专有功能
    - 在AppDelegate中添加Mac专有的窗口标题设置
    - 在ViewController中添加Mac专有的键盘快捷键支持
    - 实现Mac专有的功能按钮和交互
    - 添加Mac专有的通知系统
    - 支持以下Mac专有功能：
      • Mac风格的窗口标题
      • 键盘快捷键 (Cmd+M, Cmd+N, Cmd+W)
      • Mac原生交互体验
      • 系统集成
    - 所有Mac专有功能只在Mac Catalyst环境下启用
    - 成功构建并运行Mac Catalyst应用

--- sources/DemoMacCatalyst/DemoMacCatalyst/ViewController.swift
+++ sources/DemoMacCatalyst/DemoMacCatalyst/ViewController.swift
@@ -5,7 +5,72 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     setupButton()
+    
+    #if targetEnvironment(macCatalyst)
+    setupMacNotifications()
+    #endif
   }
+  
+  #if targetEnvironment(macCatalyst)
+  private func setupMacNotifications() {
+    // 监听Mac专有功能启用通知
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(handleMacFeaturesEnabled),
+      name: NSNotification.Name("MacFeaturesEnabled"),
+      object: nil
+    )
+  }
+  
+  @objc private func handleMacFeaturesEnabled() {
+    print("ViewController: Mac专有功能已启用")
+    // 可以在这里添加Mac专有的UI更新
+  }
+  
+  // 添加Mac专有的键盘快捷键支持
+  override var keyCommands: [UIKeyCommand]? {
+    return [
+      UIKeyCommand(
+        input: "m",
+        modifierFlags: .command,
+        action: #selector(showMacOnlyFeature),
+        discoverabilityTitle: "Mac专有功能"
+      ),
+      UIKeyCommand(
+        input: "n",
+        modifierFlags: .command,
+        action: #selector(showNewWindowFeature),
+        discoverabilityTitle: "新建窗口"
+      ),
+      UIKeyCommand(
+        input: "w",
+        modifierFlags: .command,
+        action: #selector(showCloseWindowFeature),
+        discoverabilityTitle: "关闭窗口"
+      )
+    ]
+  }
+  
+  @objc private func showNewWindowFeature() {
+    let alert = UIAlertController(title: "新建窗口", message: "这是Mac专有的新建窗口功能！\n\n通过键盘快捷键 Cmd+N 触发", preferredStyle: .alert)
+    let okAction = UIAlertAction(title: "确定", style: .default)
+    alert.addAction(okAction)
+    present(alert, animated: true)
+  }
+  
+  @objc private func showCloseWindowFeature() {
+    let alert = UIAlertController(title: "关闭窗口", message: "这是Mac专有的关闭窗口功能！\n\n通过键盘快捷键 Cmd+W 触发", preferredStyle: .alert)
+    let okAction = UIAlertAction(title: "确定", style: .default)
+    alert.addAction(okAction)
+    present(alert, animated: true)
+  }
+  
+  deinit {
+    #if targetEnvironment(macCatalyst)
+    NotificationCenter.default.removeObserver(self)
+    #endif
+  }
+  #endif
   
   private func setupButton() {
     let button = UIButton(type: .system)
@@ -20,12 +85,39 @@ class ViewController: UIViewController {
     
     view.addSubview(button)
     
+    // Mac专有功能按钮
+    #if targetEnvironment(macCatalyst)
+    let macButton = UIButton(type: .system)
+    macButton.setTitle("Mac专有功能", for: .normal)
+    macButton.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+    macButton.backgroundColor = UIColor.systemPurple
+    macButton.setTitleColor(.white, for: .normal)
+    macButton.layer.cornerRadius = 8
+    macButton.translatesAutoresizingMaskIntoConstraints = false
+    
+    macButton.addTarget(self, action: #selector(showMacOnlyFeature), for: .touchUpInside)
+    
+    view.addSubview(macButton)
+    
+    NSLayoutConstraint.activate([
+      button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      button.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -40),
+      button.widthAnchor.constraint(equalToConstant: 200),
+      button.heightAnchor.constraint(equalToConstant: 50),
+      
+      macButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      macButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 40),
+      macButton.widthAnchor.constraint(equalToConstant: 200),
+      macButton.heightAnchor.constraint(equalToConstant: 50)
+    ])
+    #else
     NSLayoutConstraint.activate([
       button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
       button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
       button.widthAnchor.constraint(equalToConstant: 200),
       button.heightAnchor.constraint(equalToConstant: 50)
     ])
+    #endif
   }
   
   @objc private func showPopoverAlert() {
@@ -51,6 +143,34 @@ class ViewController: UIViewController {
     
     present(alert, animated: true)
   }
+  
+  #if targetEnvironment(macCatalyst)
+  @objc private func showMacOnlyFeature() {
+    let alert = UIAlertController(title: "Mac专有功能", message: "🎉 这是Mac专有功能！\n\n• Mac风格的窗口标题\n• 键盘快捷键支持\n• Mac原生交互体验\n• 系统集成\n\n这个功能只在Mac上显示！", preferredStyle: .alert)
+    
+    let okAction = UIAlertAction(title: "太棒了！", style: .default) { _ in
+      print("用户体验了Mac专有功能")
+    }
+    
+    let shortcutAction = UIAlertAction(title: "查看快捷键", style: .default) { _ in
+      self.showKeyboardShortcuts()
+    }
+    
+    alert.addAction(okAction)
+    alert.addAction(shortcutAction)
+    
+    present(alert, animated: true)
+  }
+  
+  private func showKeyboardShortcuts() {
+    let alert = UIAlertController(title: "Mac键盘快捷键", message: "您可以使用以下Mac专有快捷键：\n\n• Cmd+N: 新建窗口\n• Cmd+W: 关闭窗口\n• Cmd+M: Mac专有功能\n• Cmd+Q: 退出应用\n\n这些快捷键只在Mac上有效！", preferredStyle: .alert)
+    
+    let okAction = UIAlertAction(title: "了解", style: .default)
+    alert.addAction(okAction)
+    
+    present(alert, animated: true)
+  }
+  #endif
 
 }
 
