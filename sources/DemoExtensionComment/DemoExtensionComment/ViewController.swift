b0ff3f4e863d53386912b2f3559afeafeba19770
beforeold <ibeforeold@gmail.com>
2025-10-20 10:37:39 +0800

    update DemoExtensionComment

---
+++ sources/DemoExtensionComment/DemoExtensionComment/ViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+}
+
+// MARK: - Test Extension
+// this extension provide some methods for blablabla
+// take a look now
+
+extension ViewController {
+  /// comment for this method
+  func foo() {
+
+  }
+}
