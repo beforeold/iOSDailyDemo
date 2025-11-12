6c4913055072927211b0ecb69d5777a2f83f9a37
beforeold <ibeforeold@gmail.com>
2025-11-12 16:48:20 +0800

    Add UIMenu with custom header view support
    - Add Objective-C header for UIMenu header view provider
    - Create bridging header to expose OC interface to Swift
    - Implement MenuHeaderView with UISlider
    - Create UIMenuSwiftUI wrapper to bridge UIMenu to SwiftUI
    - Update ContentView to use new UIMenu wrapper
    - Move sliderValue state to UIMenuSwiftUI to prevent ContentView refresh

---
+++ sources/DemoMenuControl/DemoMenuControl/UIMenu+UIMenuHeaderViewProvider.h
@@ -0,0 +1,12 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIMenu (UIMenuHeaderViewProvider)
+
+-(void)setHeaderViewProvider:(UIView*(^) (UIMenu*))arg1;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
