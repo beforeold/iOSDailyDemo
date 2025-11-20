73c48d9104af5e3de0a66f602d7a2fcd64294c7b
beforeold <ibeforeold@gmail.com>
2025-11-20 10:15:16 +0800

    Add PHAsset dump demo

---
+++ sources/DemoBlurEffect/DemoBlurEffect/BlurEffectProvider.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BlurEffectProvider : NSObject
+
++ (UIVisualEffect *)effectForStyleValue:(NSInteger)value;
+
+@end
+
+NS_ASSUME_NONNULL_END
