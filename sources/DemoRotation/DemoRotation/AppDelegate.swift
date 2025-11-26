ff683f05f5f8f2f5cc957320445cf36858cce1fe
beforeold <ibeforeold@gmail.com>
2025-11-26 11:46:39 +0800

    update demorotation

--- sources/DemoRotation/DemoRotation/AppDelegate.swift
+++ sources/DemoRotation/DemoRotation/AppDelegate.swift
@@ -3,11 +3,17 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+  var canRotate: Bool = false
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
     return true
+  }
+
+  func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+    print("supportedInterfaceOrientationsFor", canRotate)
+
+    return canRotate ? .all : .portrait
   }
 
   // MARK: UISceneSession Lifecycle
