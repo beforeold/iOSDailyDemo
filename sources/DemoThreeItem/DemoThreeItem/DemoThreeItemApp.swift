e95698cd7dfdb7753631ee0f04d727e2aef6fb68
beforeold <ibeforeold@gmail.com>
2025-12-08 09:35:57 +0800

    add DemoThreeItem

---
+++ sources/DemoThreeItem/DemoThreeItem/DemoThreeItemApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoThreeItemApp.swift
+//  DemoThreeItem
+//
+//  Created by beforeold on 2025/12/5.
+//
+
+import SwiftUI
+
+@main
+struct DemoThreeItemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
