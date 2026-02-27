3a3331d565b35d631607c861d4c798297c6a587e
beforeold <ibeforeold@gmail.com>
2026-02-27 10:41:49 +0800

    Create native Mobiuspace landing screen

---
+++ sources/DemoABC/DemoABC/DemoABCApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoABCApp.swift
+//  DemoABC
+//
+//  Created by beforeold on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct DemoABCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
