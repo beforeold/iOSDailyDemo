0ef04d61a8ea8f10e567f47402a12f64b3f3faa0
beforeold <ibeforeold@gmail.com>
2026-03-04 12:13:15 +0800

    add demohostbundleid

---
+++ sources/DemoHostBundleID/DemoHostBundleID/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  DemoHostBundleID
+//
+//  Created by beforeold on 3/4/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
