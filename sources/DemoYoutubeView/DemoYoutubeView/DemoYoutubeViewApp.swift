f7036e828be47dcf6341e5623fc9808fa7edfe6c
beforeold <ibeforeold@gmail.com>
2026-03-16 16:30:04 +0800

    feat: load YouTube embed in WKWebView

---
+++ sources/DemoYoutubeView/DemoYoutubeView/DemoYoutubeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoYoutubeViewApp.swift
+//  DemoYoutubeView
+//
+//  Created by beforeold on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct DemoYoutubeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
