d38b2257d46e6fa4c1d7d410f337f0bf077c876b
beforeold <ibeforeold@gmail.com>
2026-04-17 14:06:41 +0800

    chore: organize

---
+++ sources/DemoDoubleToJSON/DemoDoubleToJSON/DemoDoubleToJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoDoubleToJSONApp.swift
+//  DemoDoubleToJSON
+//
+//  Created by beforeold on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct DemoDoubleToJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sources/DemoMac/DemoMac/DemoMacApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct DemoMacApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
