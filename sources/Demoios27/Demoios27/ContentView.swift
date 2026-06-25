15f4d6f822db2c88ab430a459494e18bfaed420a
beforeold <ibeforeold@gmail.com>
2026-06-25 12:00:22 +0800

    add Demoios27

---
+++ sources/Demoios27/Demoios27/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Demoios27
+//
+//  Created by beforeold on 6/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sources/Demoios27/Demoios27/Demoios27App.swift
@@ -0,0 +1,17 @@
+//
+//  Demoios27App.swift
+//  Demoios27
+//
+//  Created by beforeold on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct Demoios27App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
